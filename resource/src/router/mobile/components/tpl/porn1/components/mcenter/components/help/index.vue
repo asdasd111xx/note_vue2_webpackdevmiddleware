4e5d67bea2fbad6d77aa0b8a8e89f294f50bddcd
chia_chen <chia_chen@mail.chungyo.net>
2020-05-21 18:18:14 +0800

    [MOD]調整CSS

--- resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/help/index.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/help/index.vue
@@ -110,11 +110,12 @@ export default {
   },
   methods: {
     handleClick(path) {
-      if (path.includes('gameintro') || path.includes('support') || path.includes('deposit')) {
-        this.msg = '正在上线 敬请期待'
-      } else {
-        this.$router.push(path)
-      }
+    //   if (path.includes('gameintro') || path.includes('support') || path.includes('deposit')) {
+    //     this.msg = '正在上线 敬请期待'
+    //   } else {
+    //     this.$router.push(path)
+    //   }
+      this.$router.push(path)
     },
     logout() {
       member.logout().then(() => { window.location.reload(); });
