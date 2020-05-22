0e265b7f48cc6cf1b31d7768c750cf1f92d10ede
kent_chou <kent_chou@mail.chungyo.net>
2020-05-22 12:39:03 +0800

    [修改]發現 預設載入圖 移除多餘組件

--- resource/src/router/mobile/components/tpl/porn1/components/search/index.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/search/index.vue
@@ -1,82 +1,81 @@
 <template>
-    <mobile-container :header-config="headerConfig">
-        <div slot="content" :class="[$style['content-wrap']]">
-            <search-info v-if="$route.params.key" :key-word="keyWord" />
-            <search-home v-else :set-key-word="setKeyWord" />
-        </div>
-    </mobile-container>
+  <mobile-container :header-config="headerConfig">
+    <div slot="content" :class="[$style['content-wrap']]">
+      <search-info v-if="$route.params.key" :key-word="keyWord" />
+      <search-home v-else :set-key-word="setKeyWord" />
+    </div>
+  </mobile-container>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-import searchHome from "./components/new/searchHome";
-import searchInfo from "./components/new/searchInfo";
+import searchHome from "./components/searchHome";
+import searchInfo from "./components/searchInfo";
 import mobileContainer from "../common/new/mobileContainer";
 
 export default {
-    components: {
-        mobileContainer,
-        searchHome,
-        searchInfo
-    },
-    data() {
+  components: {
+    mobileContainer,
+    searchHome,
+    searchInfo
+  },
+  data() {
+    return {
+      keyWord: this.$route.params.key
+    };
+  },
+  computed: {
+    ...mapGetters({
+      memInfo: "getMemInfo"
+    }),
+    headerConfig() {
+      if (this.$route.params.key) {
         return {
-            keyWord: this.$route.params.key
+          prev: true,
+          hasSearchBar: true,
+          isBackgroundGradient: true,
+          keyWord: this.$route.params.key,
+          onClick: () => {
+            this.$router.back();
+          },
+          onSearchClick: keyWord => {
+            this.setKeyWord(keyWord);
+          }
         };
-    },
-    computed: {
-        ...mapGetters({
-            memInfo: "getMemInfo"
-        }),
-        headerConfig() {
-            if (this.$route.params.key) {
-                return {
-                    prev: true,
-                    hasSearchBar: true,
-                    isBackgroundGradient: true,
-                    keyWord: this.$route.params.key,
-                    onClick: () => {
-                        this.$router.back();
-                    },
-                    onSearchClick: keyWord => {
-                        this.setKeyWord(keyWord);
-                    }
-                };
-            }
+      }
 
-            return {
-                prev: true,
-                isBackgroundGradient: true,
-                title: this.$text("S_SEARCH", "搜索视频"),
-                onClick: () => {
-                    this.$router.back();
-                }
-            };
+      return {
+        prev: true,
+        isBackgroundGradient: true,
+        title: this.$text("S_SEARCH", "搜索视频"),
+        onClick: () => {
+          this.$router.back();
         }
-    },
-    created() {
-        if (
-            !this.memInfo.config.content_rating ||
-            !this.memInfo.user.content_rating
-        ) {
-            this.$router.push("/mobile");
-        }
-    },
-    methods: {
-        setKeyWord(keyWord) {
-            if (!keyWord) {
-                return;
-            }
-
-            this.keyWord = keyWord;
-        }
+      };
     }
+  },
+  created() {
+    if (
+      !this.memInfo.config.content_rating ||
+      !this.memInfo.user.content_rating
+    ) {
+      this.$router.push("/mobile");
+    }
+  },
+  methods: {
+    setKeyWord(keyWord) {
+      if (!keyWord) {
+        return;
+      }
+
+      this.keyWord = keyWord;
+    }
+  }
 };
 </script>
 
 <style lang="scss" module>
 .content-wrap {
-    background: #f8f8f7;
+  background: #f8f8f7;
 }
-
 </style>
--- resource/src/router/mobile/components/tpl/porn1/components/videoList/index.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/videoList/index.vue
@@ -1,70 +1,70 @@
 <template>
-    <mobile-container :header-config="headerConfig">
-        <div slot="content" :class="$style['content-wrap']">
-            <video-tab :sort-id.sync="sortId" :is-single.sync="isSingle" />
-            <video-list :sort-id="sortId" :is-single="isSingle" />
-        </div>
-    </mobile-container>
+  <mobile-container :header-config="headerConfig">
+    <div slot="content" :class="$style['content-wrap']">
+      <video-tab :sort-id.sync="sortId" :is-single.sync="isSingle" />
+      <video-list :sort-id="sortId" :is-single="isSingle" />
+    </div>
+  </mobile-container>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
-import videoTab from './components/new/videoTab';
-import videoList from './components/new/videoList';
+import videoTab from './components/videoTab';
+import videoList from './components/videoList';
 import mobileContainer from '../common/new/mobileContainer';
 
 export default {
-    components: {
-        mobileContainer,
-        videoTab,
-        videoList
+  components: {
+    mobileContainer,
+    videoTab,
+    videoList
+  },
+  data() {
+    return {
+      active: +this.$route.query.sortId,
+      isOne: false
+    };
+  },
+  computed: {
+    ...mapGetters({
+      memInfo: 'getMemInfo'
+    }),
+    headerConfig() {
+      return {
+        prev: true,
+        isBackgroundGradient: true,
+        title: this.$text('S_FULL_HD_MOVIE', '全部高清影片'),
+        hasSearchBtn: true,
+        onClick: () => { this.$router.back(); }
+      };
     },
-    data() {
-        return {
-            active: +this.$route.query.sortId,
-            isOne: false
-        };
+    sortId: {
+      get() {
+        return this.active;
+      },
+      set(value) {
+        this.active = Number(value);
+      }
     },
-    computed: {
-        ...mapGetters({
-            memInfo: 'getMemInfo'
-        }),
-        headerConfig() {
-            return {
-                prev: true,
-                isBackgroundGradient: true,
-                title: this.$text('S_FULL_HD_MOVIE', '全部高清影片'),
-                hasSearchBtn: true,
-                onClick: () => { this.$router.back(); }
-            };
-        },
-        sortId: {
-            get() {
-                return this.active;
-            },
-            set(value) {
-                this.active = value;
-            }
-        },
-        isSingle: {
-            get() {
-                return this.isOne;
-            },
-            set() {
-                this.isOne = !this.isOne;
-            }
-        }
-    },
-    created() {
-        if (!this.memInfo.config.content_rating || !this.memInfo.user.content_rating) {
-            this.$router.push('/mobile');
-        }
+    isSingle: {
+      get() {
+        return this.isOne;
+      },
+      set() {
+        this.isOne = !this.isOne;
+      }
     }
+  },
+  created() {
+    if (!this.memInfo.config.content_rating || !this.memInfo.user.content_rating) {
+      this.$router.push('/mobile');
+    }
+  }
 };
 </script>
 
 <style lang="scss" module>
 .content-wrap {
-    background: #eee;
+  background: #eee;
 }
 </style>
