a6938d014ba784d916200a565dcc95ad74a7e75b
chia_chen <chia_chen@mail.chungyo.net>
2020-05-22 15:02:55 +0800

    遊戲介紹第一層頁面完成 & Detail路徑資料串接

---
+++ resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/help/page/detail/gameintro.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        1234
+    </div>
+</template>
+
+<script>
+import info from "../../json/gameintro.json";
+export default {
+    props: {
+        index: {
+            type: String,
+            required: true
+        },
+        keys: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            data: info.data[this.index].list.find(item => {
+                return +item.key === +this.keys;
+            })
+        };
+    }
+};
+</script>
+
+<style></style>
