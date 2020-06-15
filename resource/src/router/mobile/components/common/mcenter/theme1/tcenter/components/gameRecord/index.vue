71e47459f9a1cacbabaf9765aa1a651bba75603a
chia_chen <chia_chen@mail.chungyo.net>
2020-06-15 10:04:49 +0800

    [MOD]我的推廣-調整日期預設

--- resource/src/router/mobile/components/common/mcenter/theme1/tcenter/components/gameRecord/index.vue
+++ resource/src/router/mobile/components/common/mcenter/theme1/tcenter/components/gameRecord/index.vue
@@ -203,7 +203,11 @@ export default {
     },
     filters: {
         dateFormat(value) {
-            return Vue.moment(value).format("YYYY/MM/DD");
+            if(value) {
+                return Vue.moment(value).format("YYYY/MM/DD");
+            } else {
+                return '';
+            }
         }
     }
 };
