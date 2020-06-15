f59e72b6f7ff03c10acffbe5f5ff9880954d44eb
kent_chou <kent_chou@mail.chungyo.net>
2020-06-15 23:29:51 +0800

    Revert "[hotfix]彩金full"
    This reverts commit 1ab17468a68a20f3c7e30544b8cf0a8876931fc8.

--- resource/src/router/mobile/components/tpl/porn1/components/videoPlay/bouns/compontents/bonunsProcess.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/videoPlay/bouns/compontents/bonunsProcess.vue
@@ -75,7 +75,6 @@ export default {
         ],
       isClose: false,
       isInit: false,
-      isForceWait: false,
       totalAmount: 0,
       earnCoin: "",
       curMin: 0,
@@ -97,10 +96,6 @@ export default {
       }
     },
     processType() {
-      if (processType === "wait") {
-        this.isForceWait = true;
-      }
-
       this.curCoinSrc = this.coinType.find(i => i.key == this.processType).src;
     },
   },
@@ -113,11 +108,8 @@ export default {
         clearTimeout(this.timer);
         this.timer = null;
       }, 2500)
-      if (isForceWait) {
-        this.curCoinSrc = this.coinType.find(i => i.key == "wait").src;
-      } else {
-        this.curCoinSrc = this.coinType.find(i => i.key == "earn").src;
-      }
+
+      this.curCoinSrc = this.coinType.find(i => i.key == "earn").src;
       this.processType = "earn";
     },
     // 收到play跑一次進度動畫
