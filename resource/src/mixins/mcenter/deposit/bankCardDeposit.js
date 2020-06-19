30357eb521c9e7d6bf0ed26395fdd7320db0e699
kent_chou <kent_chou@mail.chungyo.net>
2020-06-19 18:04:44 +0800

    hotfix

--- resource/src/mixins/mcenter/deposit/bankCardDeposit.js
+++ resource/src/mixins/mcenter/deposit/bankCardDeposit.js
@@ -51,12 +51,7 @@ export default {
             memInfo: 'getMemInfo',
             isLoading: 'getIsLoading'
         }),
-        isWebView() {
-            return getCookie('platform') === "H" || window.location.host === "yaboxxxapp02.com";
-        },
-        isPWA() {
-            return getCookie('platform') === "G" || window.location.host === "yaboxxxapp01.com";
-        },
+
         /**
          * 所有銀行
          *
@@ -315,6 +310,12 @@ export default {
         ...mapActions([
             'actionSetIsLoading'
         ]),
+        isWebView() {
+            return getCookie('platform') === "H" || window.location.host === "yaboxxxapp02.com";
+        },
+        isPWA() {
+            return getCookie('platform') === "G" || window.location.host === "yaboxxxapp01.com";
+        },
         /**
          * 取得支付群組
          * @method getPayGroup
