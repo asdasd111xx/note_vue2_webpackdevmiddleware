acb6424ae6efe9c87e94fc5d25860486872b3bf5
kent_chou <kent_chou@mail.chungyo.net>
2020-07-11 08:09:58 +0800

    [hotfix]https://allforyouedu.com/

--- resource/src/api/config.js
+++ resource/src/api/config.js
@@ -1,6 +1,6 @@
 
 // 鸭博娱乐色站 api domain
-export const PORN_DOMAIN = 'https://heshunankang.com/api/v1';
+export const PORN_DOMAIN = 'https://allforyouedu.com/api/v1';
 
 // 色站Lucas機器 測試站 api domain
 export const S_PORN_DOMAIN = 'https://sexsite-api.in-app.cc/api/v1';
