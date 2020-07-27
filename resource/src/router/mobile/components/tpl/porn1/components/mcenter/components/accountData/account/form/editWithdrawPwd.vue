b935407750add633cde92528b5cac6b549a0a0e0
kent_chou <kent_chou@mail.chungyo.net>
2020-07-27 14:54:22 +0800

    [EY]移除不用檔案

--- resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/accountData/account/form/editWithdrawPwd.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/accountData/account/form/editWithdrawPwd.vue
@@ -1,78 +1,77 @@
 <template>
-    <edit-withdraw-pwd :old-value="oldValue" :new-value="newValue">
-        <template scope="{ hasOld, onSubmit }">
-            <div :class="[$style.wrap, 'clearfix']">
-                <template v-if="hasOld">
-                    <div :class="$style.title">{{ $text('S_OLD_PWD') }}</div>
-                    <div :class="$style['input-wrap']">
-                        <input
-                            v-for="num in 4"
-                            :key="`old-${num}`"
-                            :value="oldValue[num - 1]"
-                            :class="$style['withdraw-input']"
-                            type="text"
-                            maxlength="1"
-                            @input="onInput($event, 'oldValue', num - 1)"
-                        />
-                    </div>
-                </template>
-                <div :class="$style.title">{{ $text('S_NEW_PWD') }}</div>
-                <div :class="$style['input-wrap']">
-                    <input
-                        v-for="num in 4"
-                        :key="`new-${num}`"
-                        :value="newValue[num - 1]"
-                        :class="$style['withdraw-input']"
-                        type="text"
-                        maxlength="1"
-                        @input="onInput($event, 'newValue', num - 1)"
-                    />
-                </div>
-                <div :class="$style['btn-wrap']">
-                    <div :class="$style['btn-cancel']" @click="$emit('cancel')">{{ $text('S_CANCEL', '取消') }}</div>
-                    <div :class="$style['btn-confirm']" @click="handleSubmit(onSubmit)">{{ $text('S_CONFIRM', '確認') }}</div>
-                </div>
-                <div :class="$style['bottom-tip']">
-                    <div>{{ $text('S_LIMIT_0_TO_9') }}</div>
-                </div>
-            </div>
-        </template>
-    </edit-withdraw-pwd>
+  <div :class="[$style.wrap, 'clearfix']">
+    <template v-if="hasOld">
+      <div :class="$style.title">{{ $text("S_OLD_PWD") }}</div>
+      <div :class="$style['input-wrap']">
+        <input
+          v-for="num in 4"
+          :key="`old-${num}`"
+          :value="oldValue[num - 1]"
+          :class="$style['withdraw-input']"
+          type="text"
+          maxlength="1"
+          @input="onInput($event, 'oldValue', num - 1)"
+        />
+      </div>
+    </template>
+    <div :class="$style.title">{{ $text("S_NEW_PWD") }}</div>
+    <div :class="$style['input-wrap']">
+      <input
+        v-for="num in 4"
+        :key="`new-${num}`"
+        :value="newValue[num - 1]"
+        :class="$style['withdraw-input']"
+        type="text"
+        maxlength="1"
+        @input="onInput($event, 'newValue', num - 1)"
+      />
+    </div>
+    <div :class="$style['btn-wrap']">
+      <div :class="$style['btn-cancel']" @click="$emit('cancel')">
+        {{ $text("S_CANCEL", "取消") }}
+      </div>
+      <div :class="$style['btn-confirm']" @click="handleSubmit(onSubmit)">
+        {{ $text("S_CONFIRM", "確認") }}
+      </div>
+    </div>
+    <div :class="$style['bottom-tip']">
+      <div>{{ $text("S_LIMIT_0_TO_9") }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import editWithdrawPwd from '@/components/common/editWithdrawPwd';
 
 export default {
-    components: {
-        editWithdrawPwd
-    },
-    data() {
-        return {
-            oldValue: ['', '', '', ''],
-            newValue: ['', '', '', '']
-        };
-    },
-    methods: {
-        onInput(e, field, index) {
-            this.$set(this[field], index, e.target.value);
+  components: {
+    editWithdrawPwd
+  },
+  data() {
+    return {
+      oldValue: ['', '', '', ''],
+      newValue: ['', '', '', '']
+    };
+  },
+  methods: {
+    onInput(e, field, index) {
+      this.$set(this[field], index, e.target.value);
 
-            if (!/^[0-9]$/.test(this[field][index])) {
-                this.$set(this[field], index, '');
-            }
-        },
-        handleSubmit(submit) {
-            submit(this.value).then((response) => {
-                if (response.msg) {
-                    alert(response.msg);
-                }
-
-                if (response.status) {
-                    this.$emit('cancel');
-                }
-            });
+      if (!/^[0-9]$/.test(this[field][index])) {
+        this.$set(this[field], index, '');
+      }
+    },
+    handleSubmit(submit) {
+      submit(this.value).then((response) => {
+        if (response.msg) {
+          alert(response.msg);
         }
+
+        if (response.status) {
+          this.$emit('cancel');
+        }
+      });
     }
+  }
 };
 </script>
 
