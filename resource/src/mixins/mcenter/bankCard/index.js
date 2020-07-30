aa0b12d09a6677cb082a5b9effe4999eaf2e8c84
chia_chen <chia_chen@mail.chungyo.net>
2020-07-30 17:35:46 +0800

    [EY]卡片管理-增加Tab

---
+++ resource/src/mixins/mcenter/bankCard/index.js
@@ -0,0 +1,93 @@
+export default {
+  data() {
+    return {
+      currentPage: 'cardInfo',
+      showDetailStatus: false,
+      hideDetailIcon: false,
+      editDetailIcon: false,
+      step: 'one'
+    }
+  },
+  computed: {
+    hasRedirect() {
+      // 預設提現銀行卡添加
+      const { query } = this.$route;
+      let redirect = query.redirect;
+      if (redirect && redirect.split('-')[0]) {
+        switch (redirect.split('-')[0]) {
+          case "casino":
+          case "deposit":
+          case "withdraw":
+          case "balanceTrans":
+          case "home":
+          case "card":
+          case "mahjong":
+          case "liveStream":
+          case "videoPlay":
+          case "wallet":
+            this.currentPage = 'addCard';
+            return true;
+        }
+      }
+      return false;
+    },
+    showDetail: {
+      get() {
+        return this.showDetailStatus;
+      },
+      set(value) {
+        this.showDetailStatus = value;
+      }
+    },
+    editStatus: {
+      get() {
+        return this.editDetailIcon;
+      },
+      set(value) {
+        this.editDetailIcon = value;
+      }
+    },
+    addBankCardStep: {
+      get() {
+        return this.step;
+      },
+      set(value) {
+        this.step = value;
+      }
+    }
+  },
+  methods: {
+    changePage(value) {
+      this.currentPage = value;
+    },
+    backPre() {
+      if (this.currentPage === 'cardInfo') {
+        if (this.showDetail) {
+          this.showDetail = false;
+          return;
+        }
+        this.$router.back();
+        return;
+      }
+
+      if (this.addBankCardStep === 'two') {
+        this.step = 'one';
+        return;
+      }
+
+      if (this.$route.query && this.$route.query.redirect) {
+        if (this.$route.query.redirect === "home") {
+          this.$router.push('/mobile');
+          return;
+        } else if (this.$route.query.redirect === "liveStream") {
+          this.$router.push('/mobile/liveStream');
+          return;
+        }
+        this.$router.back();
+        return;
+      }
+
+      this.currentPage = 'cardInfo';
+    }
+  },
+}
