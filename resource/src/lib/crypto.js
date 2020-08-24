17396074a4533c898264169a8344ffae08f9f04b
kent_chou <kent_chou@mail.chungyo.net>
2020-08-24 11:26:27 +0800

    [MOD]update

--- resource/src/lib/crypto.js
+++ resource/src/lib/crypto.js
@@ -37,8 +37,9 @@ export const getEncryptImage = (info) => {
         // console.log(img)
 
         // 讀取檔案
-        let img = CryptoJS.enc.Hex.parse(response.data);
-
+        // let img = CryptoJS.enc.Hex.parse(response.data);
+        // let img = CryptoJS.enc.Base64.parse(response.data);
+        // let img = CryptoJS.enc.Utf8.parse(response.data);
         // key iv 16進位轉化
         const key = CryptoJS.enc.Utf8.parse(data.key);
         const iv = CryptoJS.enc.Utf8.parse(data.iv);
@@ -47,21 +48,19 @@ export const getEncryptImage = (info) => {
         const iv2 = CryptoJS.enc.Hex.parse(data.iv);
 
         // 解密
-        let decrypt = CryptoJS.AES.decrypt(img, key2,
-            {
-                iv: iv2,
-                mode: CryptoJS.mode.CBC,
-                padding: CryptoJS.pad.NoPadding
-            }
-        );
+        let decrypt = CryptoJS.AES.decrypt(response.data, data.key, {
+            iv: data.iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.NoPadding
+        });
 
         let base64 = '';
         base64 = CryptoJS.enc.Base64.stringify(decrypt);
 
-        console.log(decrypt);
+        console.log(decrypt.toString());
         console.log('Base64:', base64)
         // 測試
         // base64 = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png';
-        document.querySelector(`img[img-id="${info.id}"]`).src = 'data:image/jpg;base64,' + base64;
+        document.querySelector(`img[img-id="${info.id}"]`).src = 'data:image/png;base64,' + decrypt.toString();
     });
 };
