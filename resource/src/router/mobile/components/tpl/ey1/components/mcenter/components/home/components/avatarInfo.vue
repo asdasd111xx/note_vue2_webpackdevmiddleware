6e20361649410d7a169316c1681c0d4517fb4e9a
Arvin_Chen <arvin_chen@mail.chungyo.net>
2020-09-07 16:30:52 +0800

    439885 加入億元第N天，天數計算有誤

--- resource/src/router/mobile/components/tpl/ey1/components/mcenter/components/home/components/avatarInfo.vue
+++ resource/src/router/mobile/components/tpl/ey1/components/mcenter/components/home/components/avatarInfo.vue
@@ -78,14 +78,14 @@ export default {
     this.getUserViplevel();
     this.getAvatarSrc();
     if (this.loginStatus) {      
-      var oldDays=this.day = moment().diff(Vue.moment(this.memInfo.user.created_at), 'days');      
+      //var oldDays=this.day = moment().diff(Vue.moment(this.memInfo.user.created_at), 'days');      
 
       var nowUTC = moment.utc();
       var createUTC = moment.utc(this.memInfo.user.created_at);
       var newDays = nowUTC.diff(createUTC, 'days');
 
-      this.day = newDays +1;//{{ `加入亿元第${day + 1}天` }}
-      console.log('v1.0.24 ( '+ oldDays +'days ) , v1.0.26 ( ' + newDays +'days )');
+      this.day = newDays;//{{ `加入亿元第${day + 1}天` }}
+      //console.log('v1.0.24 ( '+ oldDays +'days ) , v1.0.26 ( ' + newDays +'days )');
     }
   },
   methods: {
