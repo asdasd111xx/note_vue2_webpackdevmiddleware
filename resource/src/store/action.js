30de171676f0440ab8d2ccf48ef8a8ab14f3ced1
kent_chou <kent_chou@mail.chungyo.net>
2020-09-14 17:38:54 +0800

    [MOD]暱稱限定中文

--- resource/src/store/action.js
+++ resource/src/store/action.js
@@ -1486,7 +1486,9 @@ export const actionVerificationFormData = ({ state, dispatch, commit }, data) =>
             break;
 
         case 'alias':
-            regex = /[，:;！@#$%^&*?<>()+=`|[\]{}\\"/.~\-_']*/g;
+            // regex = /[，:;！@#$%^&*?<>()+=`|[\]{}\\"/.~\-_']*/g;
+
+            regex = /[^\u3000\u3400-\u4DBF\u4E00-\u9FFF.．·]/g;
             val = val
                 .replace(regex, '')
                 .substring(0, 20);
