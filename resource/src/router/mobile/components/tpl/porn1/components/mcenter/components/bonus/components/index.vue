77f7f002b0e4839615faa93e2a055c086042193b
jie-shien <jie-shien@mail.chungyo.net>
2020-09-21 11:38:56 +0800

    bonus ui complete

---
+++ resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/bonus/components/index.vue
@@ -0,0 +1,185 @@
+<template>
+  <div>
+    <div :class="$style['menu-wrap-left']">
+      <p :class="$style['small']">累计红利总计</p>
+      <p :class="$style['big']">{{(false)? "1,234,567.00":"0.00" }} </p>
+    </div>
+    <div :class="$style['menu-wrap-right']">
+      <p :class="$style['small']">未兑现红利总计</p>
+      <p :class="$style['big']">{{(false)? "1,234,567.00":"0.00" }} </p>
+    </div>
+
+    <!-- v-if="!mainNoData"-->
+    <template v-if="!mainNoData">
+      <div :class="$style['template']">
+        <div :class="$style['amount']">
+          <div :class="$style['list']">
+                笔数：{{"22345678"}}
+          </div>
+          <hr>
+        </div>
+        <div>  <!--v-for -->
+          <div :class="$style['data-wrap']">
+            <div>
+              <div :class="$style['title']">
+                <p>{{"GNS電子"}}</p>
+              </div>
+            </div>
+            <div :class="$style['content']">
+              <div :class="$style['content-left']">
+                <p>累积红利</p>
+                <p>流水要求</p>
+                <p>累积流水</p>
+                <p>未兑现红利</p>
+              </div>
+              <div :class="$style['content-right']">
+                <p>{{"123"}}</p>
+                <p>{{"456"}}</p>
+                <p>{{"789"}}</p>
+                <p>{{"2234"}}</p>
+              </div>
+            </div>
+          </div>
+
+          
+
+          <div :class="$style['data-wrap']">
+            <div>
+              <div :class="$style['title']">
+                <p>{{"GNS電子"}}</p>
+              </div>
+            </div>
+            <div :class="$style['content']">
+              <div :class="$style['content-left']">
+                <p>累积红利</p>
+                <p>流水要求</p>
+                <p>累积流水</p>
+                <p>未兑现红利</p>
+              </div>
+              <div :class="$style['content-right']">
+                <p>{{"123"}}</p>
+                <p>{{"456"}}</p>
+                <p>{{"789"}}</p>
+                <p>{{"2234"}}</p>
+              </div>
+            </div>
+          </div>
+          
+
+          
+        </div>
+      </div>
+    </template>
+
+    <!-- v-if="mainNoData" -->
+    <template v-if="mainNoData">
+      <div v-if="mainNoData" :class="$style['no-data']">
+        {{info}}
+        <img
+          :src="$getCdnPath(`/static/image/${themeTPL}/mcenter/img_default_no_data.png`)"
+        />
+        <p>{{ $text("S_NO_BONUS", "暂时没有新的红利彩金") }}</p>
+      </div>
+    </template>
+    <infinite-loading
+      v-if="showInfinite"
+      ref="infiniteLoading"
+      @infinite="infiniteHandler"
+    >
+      <span slot="no-more" />
+      <span slot="no-results" />
+    </infinite-loading>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+import Vue from "vue";
+import InfiniteLoading from "vue-infinite-loading";
+import EST from "@/lib/EST";
+import ajax from "@/lib/ajax";
+import axios from 'axios';
+
+export default {
+  components: {
+    InfiniteLoading
+  },
+  data() {
+    return {
+      mainNoData: true,
+      isLoading: false,
+      isReceive: false,
+      showInfinite: false,
+      info: null
+    };
+  },
+  computed: {
+    ...mapGetters({
+      memInfo: "getMemInfo",
+      siteConfig: "getSiteConfig"
+    }),
+    themeTPL() {
+      return this.siteConfig.MOBILE_WEB_TPL;
+    },
+    $style() {
+      const style =
+        this[`$style_${this.siteConfig.MOBILE_WEB_TPL}`] || this.$style_porn1;
+      return style;
+    },
+  },
+  created() {
+    this.isLoading = true;
+  },
+  mounted(){
+    axios.get('/api/v1/c/gift-card').then(response=>this.info=response.data);
+
+  },
+  methods :{
+    // bonus(){
+    //   return ajax({
+    //     method:'get',
+    //     url:'/api/v1/c/gift-card',
+    //     success: response=>{
+    //       this.info=reponse
+    //     }
+    //   })
+
+    // },
+
+
+    /**
+     * 捲動加載
+     * @param {object} $state - 套件提供的方法
+     * @see { @link https://peachscript.github.io/vue-infinite-loading/#!/ }
+     */
+    infiniteHandler($state) {
+      if (this.isReceive) {
+        return;
+      }
+
+      this.isReceive = true;
+      this.getGameDetail().then(({ result }) => {   /*  method 要換成新的 API*/
+        this.isLoading = false;
+        this.isReceive = false;
+        if (result !== "ok") {
+          return;
+        }
+
+        if (
+          !this.pagination.total ||
+          this.mainListData.length === +this.pagination.total
+        ) {
+          $state.complete();
+          return;
+        }
+
+        this.showPage += 1;
+
+        $state.loaded();
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" src="../css/porn1.index.scss" module="$style_porn1"></style>
+<style lang="scss" src="../css/ey1.index.scss" module="$style_ey1"></style>
