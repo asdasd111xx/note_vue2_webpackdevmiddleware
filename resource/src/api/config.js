a115591a122fa9199535fa788f037667274e9978
chia_chen <chia_chen@mail.chungyo.net>
2020-10-14 00:07:24 +0800

    Revert "色站Domain切換"
    This reverts commit ab1f5f96de6fd39aec388895ccd6971a14e43e99.

--- resource/src/api/config.js
+++ resource/src/api/config.js
@@ -1,8 +1,8 @@
 
 // 鸭博娱乐色站 api domain
-// export const PORN_DOMAIN = 'https://foxyjizz.com/api/v1';
+export const PORN_DOMAIN = 'https://foxyjizz.com/api/v1';
 // export const PORN_DOMAIN = 'http://192.168.136.135/api/v1';
-export const PORN_DOMAIN = 'https://allforyouedu.com/api/v1';
+// export const PORN_DOMAIN = 'https://allforyouedu.com/api/v1';
 
 // 色站Lucas機器 測試站 api domain
 export const S_PORN_DOMAIN = 'https://sexsite-api.in-app.cc/api/v1';
