5bb2bc87816c4bef7bc3d9be27e672b1b603d99d
kent_chou <kent_chou@mail.chungyo.net>
2020-10-16 14:01:07 +0800

    fix merge

--- resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/help/index.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/help/index.vue
@@ -113,20 +113,10 @@ export default {
       };
     },
   },
-  created() {},
+  created() { },
   methods: {
     ...mapActions(["actionSetGlobalMessage"]),
     handleClick(item) {
-<<<<<<< HEAD
-      // 充值不開放
-=======
-      //充值不開放
->>>>>>> 052b353b4e2767995f47092da45719c123c23bb6
-      // if (item.key === "deposit") {
-      //   this.actionSetGlobalMessage({ type: "incoming" });
-      //   return;
-      // }
-
       this.$router.push(`/mobile/mcenter/help/${item.key}`);
     },
   },
