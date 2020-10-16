2a5f2a54f94f88c80dcc4ba36ba186f2fec7fc40
tina96_chen <tina96_chen@mail.chungyo.net>
2020-10-16 13:43:02 +0800

    modify-image-deposit-tutorial

--- resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/help/components/template/deposit.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/help/components/template/deposit.vue
@@ -18,44 +18,308 @@
         </div>
         <div :class="[$style['arrow-btn']]">
           <img
-            :src="$getCdnPath(`/static/image/_new/mcenter/ic_arrow_next.png`)"
+            :src="
+              $getCdnPath(`/static/image/porn1/mcenter/help/ic_arrow_next.png`)
+            "
           />
         </div>
       </div>
+
+      <div
+        slot="content"
+        v-if="categorys"
+        :class="$style['help-wrap']"
+        @click="category_isShowPop = true"
+      >
+        <div :class="$style.list">
+          <div :class="$style['list-icon']">
+            <img
+              :src="
+                $getCdnPath(
+                  `/static/image/porn1/mcenter/help/deposit/icon_${categorys[category_currentIndex].icon}.png`
+                )
+              "
+            />
+          </div>
+
+          <span> {{ categorys[category_currentIndex].name }} </span>
+          <div :class="$style['btn-next']">
+            <img
+              :src="
+                $getCdnPath(
+                  `/static/image/porn1/mcenter/help/ic_arrow_next.png`
+                )
+              "
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      v-if="category_currentIndex == 5"
+      :class="[$style['content-wrap'], 'clearfix']"
+    >
+      <div :class="[$style['tab-wrap']]">
+        <div
+          v-for="(item, index) in tabItem"
+          :key="`tab-${item.key}`"
+          :class="[
+            $style['tab-item'],
+            { [$style['is-current']]: currentTab === index },
+          ]"
+          @click="setCurrentTab(index)"
+        >
+          {{ item.text }}
+        </div>
+        <div
+          :class="$style['active-slider']"
+          :style="{
+            left: `calc(25% + 50% * ${currentTab})`,
+          }"
+        />
+      </div>
+    </div>
+
+    <div v-if="currentTab == 0">
+      <div
+        v-for="(item, index) in categorys[category_currentIndex].list"
+        :id="`q-${index}`"
+        :class="$style['cell2']"
+        :key="`q-${index}`"
+      >
+        <div :class="$style['cell2-step-title']">
+          <span
+            >步骤 {{ index + 1 }}/{{
+              categorys[category_currentIndex].list.length
+            }}</span
+          >
+        </div>
+
+        <div :class="$style['cell2-step']">
+          <img
+            :src="
+              $getCdnPath(
+                `/static/image/porn1/mcenter/help/deposit/step/${item.img}.png`
+              )
+            "
+          />
+        </div>
+      </div>
+    </div>
+
+    <div v-if="currentTab == 1">
+      <div
+        v-for="(item, index) in categorys[category_currentIndex].list2"
+        :id="`q-${index}`"
+        :class="$style['cell2']"
+        :key="`q-${index}`"
+      >
+        <div :class="$style['cell2-step-title']">
+          <span
+            >步骤 {{ index + 1 }}/{{
+              categorys[category_currentIndex].list2.length
+            }}</span
+          >
+        </div>
+
+        <div :class="$style['cell2-step']">
+          <img
+            :src="
+              $getCdnPath(
+                `/static/image/porn1/mcenter/help/deposit/step/${item.img}.png`
+              )
+            "
+          />
+        </div>
+      </div>
+    </div>
+
+    <div v-if="category_isShowPop" :class="$style['pop-wrap']">
+      <div :class="$style['pop-mask']" @click="category_isShowPop = false" />
+      <div :class="$style['pop-menu']">
+        <div :class="$style['pop-title']">
+          <span @click="category_isShowPop = false">{{
+            $text("S_CANCEL", "取消")
+          }}</span>
+          选择游戏类别
+        </div>
+        <ul :class="$style['pop-list']">
+          <li
+            v-for="(item, index) in categorys"
+            :key="index"
+            @click="setType(index)"
+          >
+            <img
+              :src="
+                $getCdnPath(
+                  `/static/image/porn1/mcenter/help/deposit/icon_${item.icon}.png`
+                )
+              "
+            />
+
+            {{ item.name }}
+            <icon
+              v-if="category_currentIndex === index"
+              :class="$style['select-icon']"
+              name="check"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div :class="$style['gotop']" @click="toTop">
+      <img
+        :src="
+          $getCdnPath(`/static/image/porn1/mcenter/help/deposit/btn_gotop.png`)
+        "
+      />
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import member from '@/api/member';
-import { getCookie } from '@/lib/cookie';
+import { mapGetters } from "vuex";
+import member from "@/api/member";
+import { getCookie } from "@/lib/cookie";
 
 export default {
-  components: {
-  },
+  components: {},
   data() {
     return {
       hasCid: false,
+      category_currentIndex: 0,
+      category_isShowPop: false,
+      currentTab: 0,
+
+      categorys: [
+        {
+          name: "网银支付",
+          icon: "bank1",
+          list: [
+            { key: 0, img: "bank1_step1" },
+            { key: 1, img: "bank1_step2" },
+          ],
+        },
+        {
+          name: "工商银行",
+          icon: "bank2",
+          list: [
+            { key: 0, img: "bank2_step1" },
+            { key: 1, img: "bank2_step2" },
+          ],
+        },
+        {
+          name: "建设银行",
+          icon: "bank3",
+          list: [
+            { key: 0, img: "bank3_step1" },
+            { key: 1, img: "bank3_step2" },
+          ],
+        },
+        {
+          name: "招商银行",
+          icon: "bank4",
+          list: [
+            { key: 0, img: "bank4_step1" },
+            { key: 1, img: "bank4_step2" },
+          ],
+        },
+        {
+          name: "微信",
+          icon: "bank5",
+          list: [
+            { key: 0, img: "bank5_step1" },
+            { key: 1, img: "bank5_step2" },
+            { key: 2, img: "bank5_step3" },
+            { key: 3, img: "bank5_step4" },
+            { key: 4, img: "bank5_step5" },
+            { key: 5, img: "bank5_step6" },
+          ],
+        },
+        {
+          name: "支付宝",
+          icon: "bank6",
+          list: [
+            { key: 0, img: "bank6_step0_1" },
+            { key: 1, img: "bank6_step0_2" },
+            { key: 2, img: "bank6_step0_3" },
+          ],
+          list2: [
+            { key: 0, img: "bank6_step1_1" },
+            { key: 1, img: "bank6_step1_2" },
+          ],
+        },
+        {
+          name: "代客充值",
+          icon: "bank7",
+          list: [
+            { key: 0, img: "bank7_step1" },
+            { key: 1, img: "bank7_step2" },
+          ],
+        },
+      ],
     };
   },
+
   mounted() {
-    this.hasCid = getCookie('cid') || false;
-    document.title = this.$text('S_HELP_CENTER', '帮助中心')
+    this.hasCid = getCookie("cid") || false;
+    document.title = this.$text("S_HELP_CENTER", "帮助中心");
+  },
+  methods: {
+    setType(index) {
+      this.currentTab = 0;
+      this.category_currentIndex = index;
+      this.category_isShowPop = false;
+    },
+    toTop() {
+      $("#mobile-wrap").animate({ scrollTop: 0 }, 1000);
+    },
+
+    setCurrentTab(index) {
+      switch (index) {
+        default:
+        case 0:
+          this.currentTab = index;
+          break;
+
+        case 1:
+          this.currentTab = index;
+          break;
+      }
+    },
   },
   computed: {
     ...mapGetters({
-      loginStatus: 'getLoginStatus'
+      loginStatus: "getLoginStatus",
     }),
+    tabItem() {
+      return [
+        {
+          key: "transfer-card",
+          text: this.$text("TRANSFER_CARD", "支付宝转卡"),
+        },
+        {
+          key: "scan-code",
+          text: this.$text("SCAN_CODE", "截屏扫码"),
+        },
+      ];
+    },
     isApp() {
-      let isApp = !!((this.$route.query && this.$route.query.app) || (this.$route.query && this.$route.query.APP))
+      let isApp = !!(
+        (this.$route.query && this.$route.query.app) ||
+        (this.$route.query && this.$route.query.APP)
+      );
       return isApp;
     },
     headerConfig() {
       return {
         prev: true,
-        onClick: () => { this.$router.back(); },
-        title: this.$text('S_HELP_CENTER', '帮助中心'),
+        onClick: () => {
+          this.$router.back();
+        },
+        title: this.$text("S_HELP_CENTER", "帮助中心"),
       };
     },
   },
--- resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/help/index.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/help/index.vue
@@ -7,11 +7,11 @@
     <div slot="content" :class="$style['help-wrap']">
       <div
         v-for="listInfo in list"
-        v-if="listInfo.needLogin ? loginStatus : true"
         :key="`list-${listInfo.name}`"
         :class="$style.list"
         @click="handleClick(listInfo)"
       >
+        <!-- v-if="listInfo.needLogin ? loginStatus : true" -->
         <div :class="$style['list-icon']">
           <img
             :src="
@@ -36,9 +36,9 @@
 </template>
 
 <script>
-import member from '@/api/member';
-import mobileContainer from '../../../common/mobileContainer';
-import { mapGetters, mapActions } from 'vuex';
+import member from "@/api/member";
+import mobileContainer from "../../../common/mobileContainer";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   components: {
@@ -48,85 +48,84 @@ export default {
     return {
       list: [
         {
-          name: '充值教程',
-          img: 'deposit',
-          key: 'deposit',
-          info: '充值极速到账',
-          needLogin: true
+          name: "充值教程",
+          img: "deposit",
+          key: "deposit",
+          info: "充值极速到账",
+          needLogin: true,
         },
         {
-          name: '提现教程',
-          img: 'withdraw',
-          key: 'withdraw',
-          info: '极速提现仅需30秒',
-          needLogin: false
+          name: "提现教程",
+          img: "withdraw",
+          key: "withdraw",
+          info: "极速提现仅需30秒",
+          needLogin: false,
         },
         {
-          name: this.$text('S_GAME_INTR', '游戏介绍'),
-          img: 'gameintro',
-          key: 'gameintro',
-          info: '主流体育彩票玩法',
-          needLogin: false
+          name: this.$text("S_GAME_INTR", "游戏介绍"),
+          img: "gameintro",
+          key: "gameintro",
+          info: "主流体育彩票玩法",
+          needLogin: false,
         },
         {
-          name: this.$text('S_TECH_SUP', '技术支持'),
-          img: 'support',
-          key: 'support',
-          info: '提供全面技术支持',
-          needLogin: false
+          name: this.$text("S_TECH_SUP", "技术支持"),
+          img: "support",
+          key: "support",
+          info: "提供全面技术支持",
+          needLogin: false,
         },
         {
-          name: this.$text('S_CONTACT_US', '联系我们'),
-          img: 'contact',
-          key: 'contact',
-          info: '为您提供全天候服务',
-          needLogin: false
+          name: this.$text("S_CONTACT_US", "联系我们"),
+          img: "contact",
+          key: "contact",
+          info: "为您提供全天候服务",
+          needLogin: false,
         },
         {
-          name: '用户须知',
-          img: 'support',
-          key: 'tips',
-          info: '确保您的权益',
-          needLogin: false
+          name: "用户须知",
+          img: "support",
+          key: "tips",
+          info: "确保您的权益",
+          needLogin: false,
         },
         {
-          name: '隐私政策',
-          img: 'support',
-          key: 'privacy',
-          info: '保障您的隐私',
-          needLogin: false
+          name: "隐私政策",
+          img: "support",
+          key: "privacy",
+          info: "保障您的隐私",
+          needLogin: false,
         },
       ],
     };
   },
   computed: {
     ...mapGetters({
-      loginStatus: 'getLoginStatus'
+      loginStatus: "getLoginStatus",
     }),
     headerConfig() {
       return {
         prev: true,
-        onClick: () => { this.$router.back(); },
-        title: this.$text('S_HELP_CENTER', '帮助中心'),
+        onClick: () => {
+          this.$router.back();
+        },
+        title: this.$text("S_HELP_CENTER", "帮助中心"),
       };
     },
   },
-  created() {
-  },
+  created() {},
   methods: {
-    ...mapActions([
-      'actionSetGlobalMessage'
-    ]),
+    ...mapActions(["actionSetGlobalMessage"]),
     handleClick(item) {
       // 充值不開放
-      if (item.key === "deposit") {
-        this.actionSetGlobalMessage({ type: "incoming" });
-        return;
-      }
+      // if (item.key === "deposit") {
+      //   this.actionSetGlobalMessage({ type: "incoming" });
+      //   return;
+      // }
 
       this.$router.push(`/mobile/mcenter/help/${item.key}`);
     },
-  }
+  },
 };
 </script>
 
