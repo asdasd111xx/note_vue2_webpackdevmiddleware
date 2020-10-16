4e0471e65ffdd5900590cf88c7afa8a81c4336ea
kent_chou <kent_chou@mail.chungyo.net>
2020-10-16 17:22:58 +0800

    debugger build

--- resource/build/build.js
+++ resource/build/build.js
@@ -2,6 +2,10 @@ require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
 // 跑建置腳本時 第一個接在build.js後的值為cdn_host ex: node build.js https://xxx.cdn.com
+
+console.log("debug here !!")
+console.log(process)
+console.log(process.argv[2])
 process.env.CDN_HOST = process.argv[2] || ''
 var ora = require('ora')
 var rm = require('rimraf')
@@ -21,13 +25,13 @@ function copyCDNOutput(callback) {
     return callback()
   }
 
-  copy(config.build.assetsRootBase, config.build.assetsRoot, function(err) {
-      if (err) throw err
-      callback()
+  copy(config.build.assetsRootBase, config.build.assetsRoot, function (err) {
+    if (err) throw err
+    callback()
   })
 }
 
-copyCDNOutput(function(err) {
+copyCDNOutput(function (err) {
   if (err) throw err
   rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     if (err) throw err
