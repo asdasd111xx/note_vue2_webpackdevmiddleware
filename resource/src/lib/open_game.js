a14984335793874517a8afd4f93cb1c22ffd9c22
kent_chou <kent_chou@mail.chungyo.net>
2020-10-19 12:27:18 +0800

    [MOD]EVO視訊內開 測試

--- resource/src/lib/open_game.js
+++ resource/src/lib/open_game.js
@@ -10,121 +10,133 @@ import store from '@/store';
  */
 // openGame({ kind: game.kind, vendor: game.vendor, code: game.code, gameType: game.type });
 export default (params, success = () => { }, fail = () => { }) => {
-    localStorage.setItem("is-open-game", true);
+  localStorage.setItem("is-open-game", true);
+  console.log(params)
+  let width = 1024;
 
-    let width = 1024;
+  // IM電競 在 IE 瀏覽器最小寬度要 1280
+  if (params.kind === 1 && params.vendor === 'tgp') {
+    width = 1280;
+  }
 
-    // IM電競 在 IE 瀏覽器最小寬度要 1280
-    if (params.kind === 1 && params.vendor === 'tgp') {
-        width = 1280;
-    }
+  const settings = {
+    vendor: '',
+    kind: '',
+    code: '',
+    width,
+    height: 768,
+    ...params
+  };
 
-    const settings = {
-        vendor: '',
-        kind: '',
-        code: '',
-        width,
-        height: 768,
-        ...params
-    };
+  if (!settings.vendor || !settings.kind) {
+    fail({ data: { msg: 'vendor 遗失' } });
+    setTimeout(() => {
+      localStorage.removeItem("is-open-game");
+    }, 1500)
+    return;
+  }
 
-    if (!settings.vendor || !settings.kind) {
-        fail({ data: { msg: 'vendor 遗失' } });
-        setTimeout(() => {
-            localStorage.removeItem("is-open-game");
-        }, 1500)
+  const { vendor, kind, code, gameType } = params;
+  const temp = { kind };
+
+  if (code) {
+    temp.code = code;
+  }
+
+  if (vendor === "bbin") {
+    temp.mobile = "1";
+  }
+
+  let newWindow = '';
+  let isWebview = getCookie("platform") === "H";
+
+  // if (vendor === "evo") {
+  // }
+
+  if (!isWebview && vendor !== 'evo') {
+    newWindow = window.open('', '', '_blank', true);
+    setTimeout(() => {
+      newWindow.location = "/game/loading/true";
+    }, 200)
+  }
+
+  return game.gameLink({
+    params: temp,
+    errorAlert: false,
+    success: (response) => {
+      console.log(response);
+      const { result, ret } = response;
+      let query = '';
+
+      if (result !== 'ok') {
+        fail({ data: { msg: 'open game fail' } });
         return;
-    }
+      }
 
-    const { vendor, kind, code, gameType } = params;
-    const temp = { kind };
+      if (!isMobileFuc() && (vendor === 'sp' || vendor === 'mg')) {
+        localStorage.setItem("open-game-link", ret + query);
+        return;
+      }
+      // 80桌參數
+      if (vendor === "lg_live" && String(kind) === "2" && gameType && gameType === "R") {
+        query += '&customize=yabo&tableType=3310';
+      }
 
-    if (code) {
-        temp.code = code;
-    }
+      if (vendor && vendor.toUpperCase() === 'ISB') {
+        query += '&allowFullScreen=false';
+      }
 
-    if (vendor === "bbin") {
-        temp.mobile = "1";
-    }
+      localStorage.setItem("open-game-link", ret.url + query);
 
-    let newWindow = '';
-    let isWebview = getCookie("platform") === "H";
+      // 開啟遊戲時強制關閉下方最愛遊戲框
+      store.dispatch('actionSetCollectionStatus', false);
 
-    if (!isWebview) {
-        newWindow = window.open('', '', '_blank', true);
-        setTimeout(() => {
-            newWindow.location = "/game/loading/true";
-        }, 200)
-    }
+      let link = `/game/${settings.vendor}/${settings.kind}`;
+      // const option = `width=${settings.width},height=${settings.height}${settings.kind === 4 ? ',scrollbars=yes, resizable=yes' : ''}`;
 
-    return game.gameLink({
-        params: temp,
-        errorAlert: false,
-        success: (response) => {
-            console.log(response);
-            const { result, ret } = response;
-            let query = '';
+      if (settings.code) {
+        link += `/${settings.code}`;
+      }
 
-            if (result !== 'ok') {
-                fail({ data: { msg: 'open game fail' } });
-                return;
+      setTimeout(() => {
+        try {
+          let isWebview = getCookie("platform") === "H";
+          if (isWebview) {
+            window.location.href = link;
+          } else {
+
+            if (vendor === "evo") {
+              localStorage.setItem('iframe-third-url', link);
+              localStorage.setItem('iframe-third-url-title', 'EVO视讯');
+              window.location.href = `/mobile/iframe/game?vendor=${settings.vendor}&kind=${settings.kind}&hasFooter=false&hasHeader=true`;
+              return;
             }
 
-            if (!isMobileFuc() && (vendor === 'sp' || vendor === 'mg')) {
-                localStorage.setItem("open-game-link", ret + query);
-                return;
-            }
-            // 80桌參數
-            if (vendor === "lg_live" && String(kind) === "2" && gameType && gameType === "R") {
-                query += '&customize=yabo&tableType=3310';
-            }
+            newWindow.location = link;
 
-            if (vendor && vendor.toUpperCase() === 'ISB') {
-                query += '&allowFullScreen=false';
-            }
-
-            localStorage.setItem("open-game-link", ret.url + query);
-
-            // 開啟遊戲時強制關閉下方最愛遊戲框
-            store.dispatch('actionSetCollectionStatus', false);
-
-            let link = `/game/${settings.vendor}/${settings.kind}`;
-            // const option = `width=${settings.width},height=${settings.height}${settings.kind === 4 ? ',scrollbars=yes, resizable=yes' : ''}`;
-
-            if (settings.code) {
-                link += `/${settings.code}`;
-            }
-
-            setTimeout(() => {
-                try {
-                    let isWebview = getCookie("platform") === "H";
-                    if (isWebview) {
-                        window.location.href = link;
-                    } else {
-                        newWindow.location = link;
-                        console.log('openWindow:', newWindow);
-                    }
-                } catch (e) {
-                    newWindow.close();
-                    console.log(e);
-                    console.log('另开视窗失败 请关闭阻挡弹出式视窗');
-                    // window.open(link, '', '_blank', true);
-                }
-                success();
-                setTimeout(() => {
-                    localStorage.removeItem('is-open-game');
-                }, 1500)
-            }, 200)
-        },
-        fail: (res) => {
-            newWindow.close();
-            console.log('launch 失敗');
-            console.log(res);
-            fail(res);
-
-            setTimeout(() => {
-                localStorage.removeItem("is-open-game");
-            }, 1500)
+            console.log('openWindow:', newWindow);
+          }
+        } catch (e) {
+          newWindow.close();
+          console.log(e);
+          console.log('另开视窗失败 请关闭阻挡弹出式视窗');
+          // window.open(link, '', '_blank', true);
         }
-    }, vendor);
+        success();
+        setTimeout(() => {
+          localStorage.removeItem('is-open-game');
+        }, 1500)
+      }, 200)
+    },
+    fail: (res) => {
+      newWindow.close();
+      console.log('launch 失敗');
+      console.log(res);
+      fail(res);
+
+      setTimeout(() => {
+        localStorage.removeItem("is-open-game");
+      }, 1500)
+    }
+  }, vendor);
 };
