09bed00ea5171f66139ebb4039271d3605889850
kent_chou <kent_chou@mail.chungyo.net>
2020-10-20 15:52:14 +0800

    [MOD]build log

--- resource/build/build.js
+++ resource/build/build.js
@@ -4,6 +4,7 @@ process.env.NODE_ENV = 'production'
 // 跑建置腳本時 第一個接在build.js後的值為cdn_host ex: node build.js https://xxx.cdn.com
 console.log("[debug]process.argv:", process.argv)
 process.env.CDN_HOST = process.argv[2] || ''
+console.log("[debug]process.env.CDN_HOST:", process.env.CDN_HOST)
 var ora = require('ora')
 var rm = require('rimraf')
 var path = require('path')
