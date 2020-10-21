e09cdfef9855d5c2ac324d8a9e6261f6a28d8406
kent_chou <kent_chou@mail.chungyo.net>
2020-10-21 11:03:53 +0800

    [MOD]維護頁 版本紀錄

--- resource/src/mixins/status.js
+++ resource/src/mixins/status.js
@@ -1,34 +1,40 @@
 import { getDoamin } from '@/lib/getCurrentDoamin';
 import store from '@/store';
+import versionFile from '@/config/version.json';
 
 // 404 500 upup no_service domain判斷
 export default {
-    data() {
-        return {
-            imgSite: '',
-            domain: {
-                domain: '',
-                site: 'porn1'
-            }
-        }
-    },
-    created() {
-        getDoamin().then((res) => {
-            this.domain = res || '';
-            this.imgSite = res && res.site ? res.site : 'porn1';
-        })
-    },
-    methods: {
-        linkToService() {
-            let on_service_url = store && store.state && store.state.webInfo && store.state.webInfo.on_service_url;
-            localStorage.setItem('serviceUrl', on_service_url || 'https://yaboxxxcs.net/chat/text/chat_1A09E4.html?skill=8ae482827375169c017375f1528b2446');
+  data() {
+    return {
+      imgSite: '',
+      domain: {
+        domain: '',
+        site: 'porn1'
+      },
+      version: ''
+    }
+  },
+  created() {
+    getDoamin().then((res) => {
+      this.domain = res || '';
+      this.imgSite = res && res.site ? res.site : 'porn1';
+    })
 
-            if (this.domain.site === "ey1") {
-                window.location.href = `/static/upup/ey1/index.html?username=${this.username}`;
-            } else {
-                window.location.href = `/static/upup/index.html?username=${this.username}`;
-            }
-            window.location.title = "在线客服";
-        },
+    if (versionFile) {
+      this.version = versionFile;
+    }
+  },
+  methods: {
+    linkToService() {
+      let on_service_url = store && store.state && store.state.webInfo && store.state.webInfo.on_service_url;
+      localStorage.setItem('serviceUrl', on_service_url || 'https://yaboxxxcs.net/chat/text/chat_1A09E4.html?skill=8ae482827375169c017375f1528b2446');
+
+      if (this.domain.site === "ey1") {
+        window.location.href = `/static/upup/ey1/index.html?username=${this.username}`;
+      } else {
+        window.location.href = `/static/upup/index.html?username=${this.username}`;
+      }
+      window.location.title = "在线客服";
     },
+  },
 };
