30b58f3ee702678460e44adcba3e0fea6978a79a
chia_chen <chia_chen@mail.chungyo.net>
2020-10-27 15:50:48 +0800

    [MOD]充值-CGPay USDT 第三方頁面開啟後重置按鈕狀態

--- resource/src/router/mobile/components/common/mcenter/theme1/deposit/components/bankCardDeposit/index.vue
+++ resource/src/router/mobile/components/common/mcenter/theme1/deposit/components/bankCardDeposit/index.vue
@@ -1167,7 +1167,6 @@ export default {
       }
     },
     curPayInfo(value) {
-      console.log(value);
       if (this.curPayInfo.payment_method_name === "代客充值") {
         this.checkSuccess = true;
       }
@@ -1665,7 +1664,8 @@ export default {
           }
           if (response.status === 'third') {
             // this.resetStatus();
-            // this.resetTimerStatus();
+            this.cryptoMoney = '--';
+            this.resetTimerStatus();
           }
         }
       });
