e242d7fba731b104f7feb4e6495afd69c4727c51
chia_chen <chia_chen@mail.chungyo.net>
2020-10-29 11:32:59 +0800

    [hotfix]額度轉讓-跳轉至提現銀行卡頁面與Tab調整 & 轉讓優惠參數調整

--- resource/src/mixins/mcenter/bankCard/index.js
+++ resource/src/mixins/mcenter/bankCard/index.js
@@ -1,96 +1,97 @@
 export default {
-    data() {
-        return {
-            isReceive: false,
+  data() {
+    return {
+      isReceive: false,
 
-            // Page 相關參數
-            currentTab: 0,
-            currentPage: "bankCardInfo",
-            isShowTab: true,
+      // Page 相關參數
+      currentTab: 0,
+      currentPage: "bankCardInfo",
+      isShowTab: true,
 
-            editDetailStatus: false,
-            isAuditStatus: false,
-            showDetailStatus: false,
-            step: "one",
-        };
-    },
-    computed: {
-        hasRedirect() {
-            // 預設提現銀行卡添加 & 電子錢包添加
-            const { query } = this.$route;
-            let redirect = query.redirect;
-            let type = query.type;
-            if (redirect && redirect.split("-")[0]) {
-                switch (redirect.split("-")[0]) {
-                    case "casino":
-                    case "deposit":
-                    case "withdraw":
-                    case "balanceTrans":
-                    case "home":
-                    case "card":
-                    case "mahjong":
-                    case "liveStream":
-                    case "videoPlay":
-                    case "wallet":
+      editDetailStatus: false,
+      isAuditStatus: false,
+      showDetailStatus: false,
+      step: "one",
+    };
+  },
+  computed: {
+    hasRedirect() {
+      // 預設提現銀行卡添加 & 電子錢包添加
+      const { query } = this.$route;
+      let redirect = query.redirect;
+      let type = query.type;
+      if (redirect && redirect.split("-")[0]) {
+        switch (redirect.split("-")[0]) {
+          case "casino":
+          case "deposit":
+          case "withdraw":
+          case "balanceTrans":
+          case "home":
+          case "card":
+          case "mahjong":
+          case "liveStream":
+          case "videoPlay":
+          case "wallet":
+          case "recharge":
 
-                        if (type && type === 'bankCard') {
-                            this.setPageStatus(0, "addBankCard", false);
-                        }
+            if (type && type === 'bankCard') {
+              this.setPageStatus(0, "addBankCard", false);
+            }
 
-                        if (type && type === 'wallet') {
-                            this.setPageStatus(1, "addWalletCard", false);
-                        }
+            if (type && type === 'wallet') {
+              this.setPageStatus(1, "addWalletCard", false);
+            }
 
-                        return true;
-                }
-            }
-            return false;
-        },
-        showDetail: {
-            get() {
-                return this.showDetailStatus;
-            },
-            set(value) {
-                this.showDetailStatus = value;
-            }
-        },
-        editStatus: {
-            get() {
-                return this.editDetailStatus;
-            },
-            set(value) {
-                this.editDetailStatus = value;
-            }
-        },
-        isAudit: {
-            get() {
-                return this.isAuditStatus;
-            },
-            set(value) {
-                this.isAuditStatus = value;
-            }
-        },
-        addBankCardStep: {
-            get() {
-                return this.step;
-            },
-            set(value) {
-                this.step = value;
-            }
+            return true;
         }
+      }
+      return false;
     },
-    created() {
-        if (localStorage.getItem('click-notification') &&
-            localStorage.getItem('add-bank-form')) {
-            this.setPageStatus(0, 'addBankCard', false)
-        }
+    showDetail: {
+      get() {
+        return this.showDetailStatus;
+      },
+      set(value) {
+        this.showDetailStatus = value;
+      }
     },
-    methods: {
-        changePage(value) {
-            this.currentPage = value;
-        },
-        changeToHistory() {
-            this.$router.push('/mobile/mcenter/historyCard');
-        }
+    editStatus: {
+      get() {
+        return this.editDetailStatus;
+      },
+      set(value) {
+        this.editDetailStatus = value;
+      }
+    },
+    isAudit: {
+      get() {
+        return this.isAuditStatus;
+      },
+      set(value) {
+        this.isAuditStatus = value;
+      }
+    },
+    addBankCardStep: {
+      get() {
+        return this.step;
+      },
+      set(value) {
+        this.step = value;
+      }
     }
+  },
+  created() {
+    if (localStorage.getItem('click-notification') &&
+      localStorage.getItem('add-bank-form')) {
+      this.setPageStatus(0, 'addBankCard', false)
+    }
+  },
+  methods: {
+    changePage(value) {
+      this.currentPage = value;
+    },
+    changeToHistory() {
+      this.$router.push('/mobile/mcenter/historyCard');
+    }
+  }
 };
