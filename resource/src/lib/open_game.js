6cacf91acead9e3c4d004c3a184d6f714dedef7a
kent_chou <kent_chou@mail.chungyo.net>
2020-10-29 16:06:09 +0800

    [hotfix]newwindow fail close

--- resource/src/lib/open_game.js
+++ resource/src/lib/open_game.js
@@ -125,7 +125,7 @@ export default (params, success = () => { }, fail = () => { }) => {
             console.log('openWindow:', newWindow);
           }
         } catch (e) {
-          newWindow.close();
+          newWindow ? newWindow.close() : '';
           console.log(e);
           console.log('另开视窗失败 请关闭阻挡弹出式视窗');
           // window.open(link, '', '_blank', true);
@@ -145,7 +145,7 @@ export default (params, success = () => { }, fail = () => { }) => {
       }, 200)
     },
     fail: (res) => {
-      newWindow.close();
+      newWindow ? newWindow.close() : '';
       console.log('launch 失敗');
       console.log(res);
       fail(res);
