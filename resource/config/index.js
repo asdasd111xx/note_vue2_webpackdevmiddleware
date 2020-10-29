8247393c709c02cd2607c9ff9af43a87eb5301a0
kent_chou <kent_chou@mail.chungyo.net>
2020-10-29 10:27:37 +0800

    test

--- resource/config/index.js
+++ resource/config/index.js
@@ -1,6 +1,6 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 const path = require("path");
-
+// test
 const outputDirName = process.env.CDN_HOST
   ? process.env.CDN_HOST.split("://")[1]
   : "www";
