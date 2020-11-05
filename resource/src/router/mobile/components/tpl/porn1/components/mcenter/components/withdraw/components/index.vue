c8b5123f3ee2071101d04bfeec83246602017430
俊賢 <chun-hsien@mail.chungyo.net>
2020-11-05 16:01:14 +0800

    [MOD] 修正紅利bug

--- resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/withdraw/components/index.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/withdraw/components/index.vue
@@ -512,11 +512,11 @@
       </div>
 
       <div :class="$style['tips']">
-        {{
-          allWithdrawAccount && allWithdrawAccount.length !== 0
-            ? "为了方便您快速提现，请先将所有场馆钱包金额回收至中心钱包"
-            : "请先绑定一张银行卡，用于收款"
-        }}
+        <div v-if="allWithdrawAccount && allWithdrawAccount.length !== 0">
+          为了方便您快速提现，请先将所有场馆钱包金额回收至中心钱包<br />
+          可提现金额会扣除未兑现红利总计
+        </div>
+        <div v-else>请先绑定一张银行卡，用于收款</div>
       </div>
     </template>
 
