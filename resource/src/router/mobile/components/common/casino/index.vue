b22ce053c94ee07f44fcc8b7c69f88cef9f864af
chia_chen <chia_chen@mail.chungyo.net>
2020-11-19 09:14:12 +0800

    [MOD]遊戲搜索更改為全部

--- resource/src/router/mobile/components/common/casino/index.vue
+++ resource/src/router/mobile/components/common/casino/index.vue
@@ -412,10 +412,16 @@ export default {
         return;
       }
 
+      let _params = { ...this.paramsData };
+      // 進到搜索頁面，Label = '' 為搜索全部
+      if (this.isShowSearch) {
+        _params = { ..._params, label: "" };
+      }
+
       const gameApiInfo = {
         url: gameList,
         params: {
-          ...this.paramsData,
+          ..._params,
           vendor: this.vendor
         }
       };
