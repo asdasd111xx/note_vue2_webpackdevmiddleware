ea60d7cc8416c770ded6b686a1a8a15b87548c30
chia_chen <chia_chen@mail.chungyo.net>
2020-11-25 08:40:52 +0800

    [MOD]提現-首次進入不能下滑問題

--- resource/src/router/mobile/components/tpl/ey1/components/mcenter/components/withdraw/index.vue
+++ resource/src/router/mobile/components/tpl/ey1/components/mcenter/components/withdraw/index.vue
@@ -66,6 +66,7 @@ export default {
 .content-wrap {
   background-color: $main_background_white1;
   color: #414655;
-  min-height: calc(100vh);
+  min-height: 100vh;
+  overflow-y: scroll;
 }
 </style>
