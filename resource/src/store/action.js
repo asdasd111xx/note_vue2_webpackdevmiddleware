28d048a78082c3dab6766ce63eebe7bbabbc16f6
kent_chou <kent_chou@mail.chungyo.net>
2020-12-08 14:09:47 +0800

    version

--- resource/src/store/action.js
+++ resource/src/store/action.js
@@ -1802,8 +1802,10 @@ export const actionSetWebDomain = ({ commit }) =>
         site: "porn1"
       };
 
-      console.log("[conf/domain]:", res.data);
-      console.log(version)
+      console.log("[conf/domain]:", {
+        ...res.data,
+        version: version.find(i => i.site === res.data.site).version
+      });
       const site = (res && res.data && String(res.data.site)) || "";
       const domain = (res && res.data && String(res.data.domain)) || "";
       result["site"] = site;
