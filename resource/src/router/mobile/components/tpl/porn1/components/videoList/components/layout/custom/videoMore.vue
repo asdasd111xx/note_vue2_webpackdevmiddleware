7c2ddc85d895747e829e9f8209ed82f05202cacf
chia_chen <chia_chen@mail.chungyo.net>
2020-12-14 09:42:13 +0800

    [MOD]小豬視頻版面調整

--- resource/src/router/mobile/components/tpl/porn1/components/videoList/components/layout/custom/videoMore.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/videoList/components/layout/custom/videoMore.vue
@@ -96,7 +96,6 @@ export default {
         centeredSlidesBounds: true,
         spaceBetween: 15,
         slidesOffsetBefore: 20,
-        slidesOffsetAfter: 20,
         freeMode: true
       };
     },
@@ -268,7 +267,6 @@ export default {
   background: $main_white_color1;
   max-width: $mobile_max_width;
   width: 100%;
-  padding-right: 40px;
   position: fixed;
   top: 43px;
   z-index: 2;
