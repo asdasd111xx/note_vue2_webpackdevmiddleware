5ce8afa68a6c3f68da652ede9ceb47278b7f86dc
kent_chou <kent_chou@mail.chungyo.net>
2020-12-15 10:57:21 +0800

    [MOD]launch 頁面共用

--- resource/src/router/mobile/components/tpl/porn1/components/launch/index.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/launch/index.vue
@@ -1,93 +1,14 @@
 <template>
-  <div :class="$style['container']">
-    <img src="/static/image/_new/common/loading.png" />
-    <div :class="$style['timer']" @click="click">
-      <span>{{ sec }}</span>
-      <span>|</span>
-      <span>跳过</span>
-    </div>
-  </div>
+  <launch />
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import { getCookie, setCookie } from '@/lib/cookie';
-
+import launch from '@/router/mobile/components/common/launch'
 export default {
-  data() {
-    return {
-      timer: 0,
-      sec: 3
-    }
-  },
-  computed: {
-    // ...mapGetters({
-    //   memInfo: "getMemInfo"
-    // }),
-  },
-  created() {
-    //   初始化
-    localStorage.removeItem('home-menu-type');
-    localStorage.removeItem('is-open-game');
-    localStorage.removeItem('do-not-show-home-post');
-    localStorage.removeItem('is-show-popup-announcement');
-    localStorage.removeItem('mobile-slider');
-  },
-  mounted() {
-    this.timer = setInterval(() => {
-      if (this.sec === 0) {
-        clearInterval(this.timer);
-        this.timer = null;
-        this.$router.push('/mobile');
-      }
-      this.sec -= 1;
-    }, 1000)
-  },
-  beforeDestroy() {
-    clearInterval(this.timer);
-    this.timer = null;
-  },
-  methods: {
-    click() {
-      clearInterval(this.timer);
-      this.timer = null;
-      this.$nextTick(() => {
-        this.$router.push('/mobile');
-      });
-    }
-  },
+  components: {
+    launch
+  }
 };
 </script>
-
 <style lang="scss" module>
-@import "~@/css/variable.scss";
-.container {
-  position: relative;
-  height: 100vh;
-
-  > img {
-    width: 100%;
-    height: 100%;
-  }
-}
-
-@media (orientation: landscape) {
-  .container > img {
-    height: unset;
-  }
-}
-
-.timer {
-  position: absolute;
-  top: 45px;
-  right: 14px;
-  background-color: #b3b3b3;
-  width: 80px;
-  border-radius: 10px;
-  color: white;
-  height: 24px;
-  line-height: 24px;
-  font-size: 14px;
-  text-align: center;
-}
 </style>
