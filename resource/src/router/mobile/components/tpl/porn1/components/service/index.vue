e4367f1235da818a8e284b7f76af33b086fbe8ea
kent_chou <kent_chou@mail.chungyo.net>
2021-01-06 12:18:04 +0800

    [MOD]調整共用客服元件/圖片

--- resource/src/router/mobile/components/tpl/porn1/components/service/index.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/service/index.vue
@@ -368,7 +368,6 @@ div.container {
     background-image: url("/static/image/porn1/service/service_card.png");
     background-size: auto 100%;
     background-position: top 0 right 0;
-    height: 100%;
     background-repeat: no-repeat;
 
     > div {
@@ -397,6 +396,13 @@ div.container {
   margin-top: 20px;
   background-image: -webkit-linear-gradient(16deg, #8ab3e2, #b5d0ef);
   background-image: linear-gradient(74deg, #8ab3e2, #b5d0ef);
+
+  > div:first-child {
+    background: url("/static/image/common/service/bg_service02.png");
+    background-size: auto 100%;
+    background-position: top 0 right 0;
+    background-repeat: no-repeat;
+  }
 }
 
 .btn-next {
