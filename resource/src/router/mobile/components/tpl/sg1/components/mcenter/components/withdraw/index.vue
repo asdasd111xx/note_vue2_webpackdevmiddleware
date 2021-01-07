268dca19f45f2ae10c5a6d2e7cb65f975dd0d71b
chia_chen <chia_chen@mail.chungyo.net>
2021-01-07 15:41:38 +0800

    [MOD]提現-進入幫助中心不需暫存金額 (FB:466471)

--- resource/src/router/mobile/components/tpl/sg1/components/mcenter/components/withdraw/index.vue
+++ resource/src/router/mobile/components/tpl/sg1/components/mcenter/components/withdraw/index.vue
@@ -9,56 +9,69 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
-import ajax from '@/lib/ajax';
-import balanceBack from '@/router/mobile/components/tpl/porn1/components/mcenter/components/common/balanceBack';
-import mobileContainer from '../../../common/mobileContainer';
-import withdraw from '@/router/mobile/components/tpl/porn1/components/mcenter/components/withdraw/components/index'
+import { mapGetters, mapActions } from "vuex";
+import ajax from "@/lib/ajax";
+import balanceBack from "@/router/mobile/components/tpl/porn1/components/mcenter/components/common/balanceBack";
+import mobileContainer from "../../../common/mobileContainer";
+import withdraw from "@/router/mobile/components/tpl/porn1/components/mcenter/components/withdraw/components/index";
 
 export default {
   data() {
-    return {
-    }
+    return {};
   },
   components: {
-    pageLoading: () => import(/* webpackChunkName: 'pageLoading' */ '@/router/mobile/components/common/pageLoading'),
+    pageLoading: () =>
+      import(
+        /* webpackChunkName: 'pageLoading' */ "@/router/mobile/components/common/pageLoading"
+      ),
     mobileContainer,
     withdraw,
     balanceBack
   },
   computed: {
     ...mapGetters({
-      siteConfig: 'getSiteConfig',
+      siteConfig: "getSiteConfig"
     }),
     headerConfig() {
       return {
         prev: true,
-        onClick: () => { this.$router.back(); },
-        title: this.$text('S_WITHDRAWAL_TEXT', '提现'),
+        onClick: () => {
+          this.$router.back();
+        },
+        title: this.$text("S_WITHDRAWAL_TEXT", "提现"),
         hasHelp: {
-          type: 'withdraw',
-          url: '/mobile/mcenter/help/withdraw',
+          type: "withdraw",
+          url: "/mobile/mcenter/help/withdraw",
           func: () => {
             this.saveCurrentValue(true);
           }
-        },
+        }
       };
-    },
+    }
   },
   methods: {
     // 暫存目前選擇的銀行卡及金額
     saveCurrentValue(fromRule) {
-      localStorage.setItem("tmp_w_selectedCard", JSON.stringify(this.$refs.withdraw.selectedCard));
-      localStorage.setItem("tmp_w_withdrawCurrency", JSON.stringify(this.$refs.withdraw.withdrawCurrency));
-      localStorage.setItem("tmp_w_amount", this.$refs.withdraw.withdrawValue);
-      localStorage.setItem("tmp_w_actualAmount", JSON.stringify(this.$refs.withdraw.actualMoney));
-      localStorage.setItem("tmp_w_withdrawPwd", this.$refs.withdraw.withdrawPwd);
+      localStorage.setItem(
+        "tmp_w_selectedCard",
+        JSON.stringify(this.$refs.withdraw.selectedCard)
+      );
+      localStorage.setItem(
+        "tmp_w_withdrawCurrency",
+        JSON.stringify(this.$refs.withdraw.withdrawCurrency)
+      );
+      // localStorage.setItem("tmp_w_amount", this.$refs.withdraw.withdrawValue);
+      // localStorage.setItem("tmp_w_actualAmount", JSON.stringify(this.$refs.withdraw.actualMoney));
+      localStorage.setItem(
+        "tmp_w_withdrawPwd",
+        this.$refs.withdraw.withdrawPwd
+      );
       if (fromRule) {
         localStorage.setItem("tmp_w_rule", "1");
       }
-    },
-  },
-}
+    }
+  }
+};
 </script>
 
 <style lang="scss" module>
