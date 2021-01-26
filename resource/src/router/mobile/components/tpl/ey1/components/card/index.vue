0b6bf96a6c192438091c305c8b264e26213dba3a
kent_chou <kent_chou@mail.chungyo.net>
2021-01-26 11:07:32 +0800

    [MOD]億元樂遊棋牌特例

--- resource/src/router/mobile/components/tpl/ey1/components/card/index.vue
+++ resource/src/router/mobile/components/tpl/ey1/components/card/index.vue
@@ -33,9 +33,18 @@ export default {
       const target = this.memInfo.vendors.find(
         item => item.vendor === vendor && item.kind === 5
       );
+      let title = "";
+      if (target && target.vendor === "leg" && target.kind === 5) {
+        // 454045
+        // 【文案需求】鴨博娛樂 - 首頁 - 遊戲入口全面調整（C-3704）
+        title = "LEG乐游";
+      } else {
+        title = target ? target.alias : "";
+      }
+
       return {
         prev: true,
-        title: target ? target.alias : "",
+        title: title,
         hasSearchBtn: true,
         onClick: () => {
           this.$router.back();
