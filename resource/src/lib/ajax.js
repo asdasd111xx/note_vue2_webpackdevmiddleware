3395d75c8e0c5975234c3b0079747669bb20f46c
kent_chou <kent_chou@mail.chungyo.net>
2021-01-27 09:25:26 +0800

    [MOD]timeout 20sec

--- resource/src/lib/ajax.js
+++ resource/src/lib/ajax.js
@@ -1,95 +1,103 @@
-import { getCookie, setCookie } from '@/lib/cookie';
+import { getCookie, setCookie } from "@/lib/cookie";
 
-import axios from 'axios';
-import querystring from 'querystring';
+import axios from "axios";
+import querystring from "querystring";
 
 export default ({
-    method = '',
-    url = '',
-    params = {},
-    success = () => { },
-    fail = () => { },
-    headers = () => { },
-    timeout = 30000,
-    errorAlert = true,
-    cache = false,
-    redirect = true
+  method = "",
+  url = "",
+  params = {},
+  success = () => {},
+  fail = () => {},
+  headers = () => {},
+  timeout = 20000,
+  errorAlert = true,
+  cache = false,
+  redirect = true
 }) => {
-    const obj = {
-        method,
-        url,
-        timeout,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    };
-
-    if (method === 'post' || method === 'put' || method === 'delete') {
-        obj.data = querystring.stringify(params);
-    } else {
-        obj.params = params;
+  const obj = {
+    method,
+    url,
+    timeout,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
     }
+  };
 
-    // 強制更新
-    if (!cache) {
-        obj.headers['If-Modified-Since'] = '0';
-    }
+  if (method === "post" || method === "put" || method === "delete") {
+    obj.data = querystring.stringify(params);
+  } else {
+    obj.params = params;
+  }
 
-    return axios(obj).then((response) => {
-        if (response.data.result === 'ok') {
-            success(response.data);
-            headers(response.headers, response.data);
+  // 強制更新
+  if (!cache) {
+    obj.headers["If-Modified-Since"] = "0";
+  }
+
+  return axios(obj)
+    .then(response => {
+      if (response.data.result === "ok") {
+        success(response.data);
+        headers(response.headers, response.data);
+      }
+      if (response.data.result === "error") {
+        if (response.data.msg && errorAlert) {
+          console.log(`${response.data.msg}(${response.data.code})`);
+          alert(`${response.data.msg}`);
         }
-        if (response.data.result === 'error') {
-            if (response.data.msg && errorAlert) {
-                console.log(`${response.data.msg}(${response.data.code})`);
-                alert(`${response.data.msg}`);
-            }
 
-            fail(response);
-        }
-        return response.data;
+        fail(response);
+      }
+      return response.data;
     })
-        .catch((error) => {
-            const errorResponse = error && error.response && error.response.data;
+    .catch(error => {
+      const errorResponse = error && error.response && error.response.data;
 
-            if (process.env.NODE_ENV === "development") {
-                console.log("[/api request]")
-                console.log(errorResponse)
-            }
+      if (process.env.NODE_ENV === "development") {
+        console.log("[/api request]");
+        console.log(errorResponse);
+      }
 
-            if (errorResponse && errorResponse.msg && errorAlert) {
-                alert(`${errorResponse.msg}`);
-            }
+      if (typeof error === "string") {
+        window.location.href = "/timeout";
+        return;
+      }
 
-            // 收到重新登入需要導向登入頁面
-            if (errorResponse) {
-                if (errorResponse.code === "M00001") {
-                    if (getCookie('cid')) {
-                        alert(`${errorResponse.msg}`);
-                    }
+      if (errorResponse && errorResponse.msg && errorAlert) {
+        alert(`${errorResponse.msg}`);
+      }
 
-                    setCookie('cid', '');
-                    setCookie('y_token', '');
-                    setCookie('aid', '');
+      // 收到重新登入需要導向登入頁面
+      if (errorResponse) {
+        if (errorResponse.code === "M00001") {
+          if (getCookie("cid")) {
+            alert(`${errorResponse.msg}`);
+          }
 
-                    window.location.reload(true);
-                    window.location.href = '/mobile/login';
-                    return;
-                }
-                // 維護中導向
-                if (errorResponse.code === "M00002" &&
-                    !window.location.href.includes('upup') &&
-                    redirect) {
-                    window.location.href = '/upup';
-                    return;
-                }
-            }
+          setCookie("cid", "");
+          setCookie("y_token", "");
+          setCookie("aid", "");
 
-            if (error && error.response) {
-                fail(error.response);
-            } else {
-                fail(error.toString());
-            }
-        });
+          window.location.reload(true);
+          window.location.href = "/mobile/login";
+          return;
+        }
+        // 維護中導向
+        if (
+          errorResponse.code === "M00002" &&
+          !window.location.href.includes("upup") &&
+          redirect
+        ) {
+          window.location.href = "/upup";
+          return;
+        }
+      }
+
+      if (error && error.response) {
+        fail(error.response);
+      } else {
+        fail(error.toString());
+      }
+    });
 };
--- resource/src/lib/ajax2.js
+++ resource/src/lib/ajax2.js
@@ -1,57 +1,66 @@
-import { apiErrorAlarm } from './errorConsole';
-import axios from 'axios';
+import { apiErrorAlarm } from "./errorConsole";
+import axios from "axios";
 
 export default ({
-    method = 'get',
-    url = '',
-    params = {},
-    timeout = 30000,
-    cache = false,
-    responseInfo = false,
-    errorAlert = true,
-    fail = () => { },
+  method = "get",
+  url = "",
+  params = {},
+  timeout = 20000,
+  cache = false,
+  responseInfo = false,
+  errorAlert = true,
+  fail = () => {}
 }) => {
-    const obj = {
-        method,
-        url,
-        timeout,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
-    if (method === 'post' || method === 'put' || method === 'delete') {
-        obj.data = params;
-    } else {
-        obj.params = params;
+  const obj = {
+    method,
+    url,
+    timeout,
+    headers: {
+      "Content-Type": "application/json"
     }
+  };
 
-    // 強制更新
-    if (!cache) {
-        obj.headers['If-Modified-Since'] = '0';
-    }
+  if (method === "post" || method === "put" || method === "delete") {
+    obj.data = params;
+  } else {
+    obj.params = params;
+  }
 
-    return axios(obj).then((response) => {
-        if (response.data.result === 'error') {
-            if (response.data.msg && errorAlert) {
-                alert(`${response.data.msg} ${response.data.code ? `(${response.data.code})` : ''}`);
-            }
-            fail(response);
-            apiErrorAlarm({ url, response });
+  // 強制更新
+  if (!cache) {
+    obj.headers["If-Modified-Since"] = "0";
+  }
+
+  return axios(obj)
+    .then(response => {
+      if (response.data.result === "error") {
+        if (response.data.msg && errorAlert) {
+          alert(
+            `${response.data.msg} ${
+              response.data.code ? `(${response.data.code})` : ""
+            }`
+          );
         }
+        fail(response);
+        apiErrorAlarm({ url, response });
+      }
 
-        return responseInfo ? response : response.data;
+      return responseInfo ? response : response.data;
     })
-        .catch((error) => {
-            if (error.response && error.response.data.msg && errorAlert) {
-                alert(`${error.response.data.msg} ${error.response.data.code ? `(${error.response.data.code})` : ''}`);
-            }
+    .catch(error => {
+      if (error.response && error.response.data.msg && errorAlert) {
+        alert(
+          `${error.response.data.msg} ${
+            error.response.data.code ? `(${error.response.data.code})` : ""
+          }`
+        );
+      }
 
-            if (error.response) {
-                fail(error.response);
-                return error.response;
-            }
+      if (error.response) {
+        fail(error.response);
+        return error.response;
+      }
 
-            return error.toString();
-        });
+      return error.toString();
+    });
 };
