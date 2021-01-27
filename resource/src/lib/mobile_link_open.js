6aa7c53a05bd08b22ec09bb364b23ded99bac222
kent_chou <kent_chou@mail.chungyo.net>
2021-01-27 20:13:15 +0800

    [hotfix]客服連結外開

--- resource/src/lib/mobile_link_open.js
+++ resource/src/lib/mobile_link_open.js
@@ -142,11 +142,8 @@ export default target => {
 
     if (linkTo.includes("service")) {
       let url = store.state.webInfo.on_service_url;
-      newWindow = window.open(
-        url,
-        "",
-        `toolbar=yes, width=${store.state.webInfo.on_service_w}, height=${store.state.webInfo.on_service_h}`
-      );
+      console.log(url);
+      window.open(url);
 
       // 在線客服流量分析事件
       window.dataLayer.push({
