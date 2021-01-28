b079621cf8c19ae7ea0a7794ab18407678b7e9f2
chia_chen <chia_chen@mail.chungyo.net>
2021-01-28 15:05:13 +0800

    [MOD]鴨/絲-下級統計-今日/昨日/近30日 FB: 471393

--- resource/src/mixins/mcenter/management/allLevelStatistics.js
+++ resource/src/mixins/mcenter/management/allLevelStatistics.js
@@ -1,112 +1,124 @@
-import Vue from 'vue';
-import { API_MANAGEMENT_LEVEL_LIST } from '@/config/api';
-import ajax from '@/lib/ajax';
-import EST from '@/lib/EST';
+import { mapGetters, mapActions } from "vuex";
+import CommonDate from "@/lib/commonDate";
+import goLangApiRequest from "@/api/goLangApiRequest";
 
 export default {
-    props: {
-        period: {
-            type: String,
-            required: true
-        }
-    },
-    data() {
-        const estToday = EST(new Date(), '', true);
-        const days = estToday.getDay() || 7; // 取得當前是星期幾，用來取得本週與上週的日期
-        const week = [
-            new Date(Vue.moment(estToday).add(-(days - 1), 'days')),
-            estToday
-        ];
-        const last = [
-            new Date(Vue.moment(estToday).add(-((days - 1) + 7), 'days')),
-            new Date(Vue.moment(estToday).add(-((days - 1) + 1), 'days'))
-        ];
-        const thirty = [
-            new Date(Vue.moment(estToday).add(-29, 'days')),
-            estToday
-        ];
-        const date = [...week];
-
-        return {
-            week,
-            last,
-            thirty,
-            date,
-            levelTrans: {
-                1: 'S_FIRST_LEVEL_FRIEND',
-                2: 'S_SECOND_LEVEL_FRIEND',
-                3: 'S_THIRD_LEVEL_FRIEND',
-                4: 'S_FOURTH_LEVEL_FRIEND',
-                5: 'S_FIFTH_LEVEL_FRIEND'
-            },
-            levelList: [],
-            allTotal: {}
-        };
-    },
-    watch: {
-        period() {
-            switch (this.period) {
-                case 'week':
-                    this.date = [...this.week];
-                    break;
-                case 'last':
-                    this.date = [...this.last];
-                    break;
-                case 'thirty':
-                    this.date = [...this.thirty];
-                    break;
-                default:
-                    break;
-            }
-
-            this.getLevelList();
-        }
-    },
-    created() {
-        this.getLevelList();
-    },
-    methods: {
-        /**
-         * 取得各級好友的資訊
-         * @method getLevelList
-         */
-        getLevelList() {
-            this.levelList = [];
-
-            ajax({
-                method: 'get',
-                url: API_MANAGEMENT_LEVEL_LIST,
-                params: {
-                    start_at: Vue.moment(this.date[0]).format('YYYY-MM-DD 00:00:00-04:00'),
-                    end_at: Vue.moment(this.date[1]).format('YYYY-MM-DD 23:59:59-04:00')
-                },
-                success: ({ result, ret }) => {
-                    if (result !== 'ok') {
-                        return;
-                    }
-
-                    if (!ret.length) {
-                        alert(this.$text('S_NO_DATA_TPL'));
-                        return;
-                    }
-
-                    this.allTotal = {
-                        total: 0,
-                        register: 0,
-                        deposit: 0,
-                        withdraw: 0
-                    };
-
-                    this.levelList = ret.map((info) => {
-                        this.allTotal.total += +info.total;
-                        this.allTotal.register += +info.register;
-                        this.allTotal.deposit += +info.deposit;
-                        this.allTotal.withdraw += +info.withdraw;
-
-                        return info;
-                    });
-                }
-            });
-        }
+  props: {
+    period: {
+      type: String,
+      required: true
     }
+  },
+  data() {
+    return {
+      date: "",
+      levelTrans: {
+        1: "S_FIRST_LEVEL_FRIEND",
+        2: "S_SECOND_LEVEL_FRIEND",
+        3: "S_THIRD_LEVEL_FRIEND",
+        4: "S_FOURTH_LEVEL_FRIEND",
+        5: "S_FIFTH_LEVEL_FRIEND"
+      },
+      levelList: [],
+      allTotal: {}
+    };
+  },
+  computed: {
+    ...mapGetters({
+      siteConfig: "getSiteConfig"
+    }),
+    themeTPL() {
+      return this.siteConfig.MOBILE_WEB_TPL;
+    },
+    commonDate() {
+      return CommonDate.getDate();
+    }
+  },
+  watch: {
+    period() {
+      switch (this.period) {
+        case "today":
+          this.date = [this.commonDate.today_start, this.commonDate.today_end];
+          break;
+
+        case "yesterday":
+          this.date = [
+            this.commonDate.yesterday_start,
+            this.commonDate.yesterday_end
+          ];
+          break;
+
+        case "week":
+          this.date = [...this.commonDate.week];
+          break;
+
+        case "last":
+          this.date = [...this.commonDate.lastWeek];
+          break;
+
+        case "thirty":
+          this.date = [...this.commonDate.thirty];
+          break;
+
+        default:
+          break;
+      }
+
+      this.getLevelList();
+    }
+  },
+  created() {
+    this.date =
+      this.themeTPL === "ey1"
+        ? [...this.commonDate.week]
+        : [this.commonDate.today_start, this.commonDate.today_end];
+
+    this.getLevelList();
+  },
+  methods: {
+    /**
+     * 取得各級好友的資訊
+     * @method getLevelList
+     */
+    getLevelList() {
+      this.levelList = [];
+
+      return goLangApiRequest({
+        method: "get",
+        url: `${this.siteConfig.YABO_GOLANG_API_DOMAIN}/xbb/Stats/Player/Friends/TransReport`,
+        params: {
+          lang: "zh-cn",
+          startAt: this.date[0],
+          endAt: this.date[1]
+        }
+      })
+        .then(res => {
+          const { data, status, errorCode } = res;
+          if (status !== "000" || errorCode !== "00") {
+            return;
+          }
+
+          this.allTotal = {
+            total: 0,
+            register: 0,
+            deposit: 0,
+            withdraw: 0
+          };
+
+          this.levelList = data.map(info => {
+            this.allTotal.total += +info.total;
+            this.allTotal.register += +info.register;
+            this.allTotal.deposit += +info.deposit;
+            this.allTotal.withdraw += +info.withdraw;
+            return info;
+          });
+        })
+        .catch(error => {
+          const { msg, code } = error.response.data;
+          this.actionSetGlobalMessage({
+            msg
+          });
+        });
+    }
+  }
 };
