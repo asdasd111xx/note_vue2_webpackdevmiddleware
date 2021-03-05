48d7abaf6f8aa546bbfc9ccc4ea423f72d7324cc
chia_chen <chia_chen@mail.chungyo.net>
2021-03-05 16:38:06 +0800

    [MOD]卡片管理-購寶先隱藏一鍵綁定的功能需求

--- resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/bankCard/components/wallet/addCard.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/bankCard/components/wallet/addCard.vue
@@ -753,8 +753,8 @@ export default {
       this.selectTarget.walletId = bank.id;
       this.selectTarget.swiftCode = bank.swift_code;
 
-      // 僅 CGpay && 購寶 有一鍵綁定
-      if ([21, 37].includes(this.selectTarget.walletId)) {
+      // 僅 CGpay 有一鍵綁定 (購寶等之後才有)
+      if ([21].includes(this.selectTarget.walletId)) {
         this.selectTarget.oneClickBindingMode = true;
       } else {
         this.selectTarget.oneClickBindingMode = false;
@@ -867,27 +867,45 @@ export default {
 
       // 購寶
       if (id === 37) {
+        // 等購寶有一鍵綁定需求才註解掉
+        // let _data = {
+        //   key: "goBao",
+        //   text: `试试`,
+        //   hasCallback: true,
+        //   dataObj: {
+        //     text: bindingMode ? `扫码绑定` : `一键绑定`,
+        //     cb: () => {
+        //       if (bindingMode) {
+        //         this.selectTarget.oneClickBindingMode = false;
+        //       } else {
+        //         this.selectTarget.oneClickBindingMode = true;
+        //       }
+        //     }
+        //   }
+        // };
+        //  this.walletTipInfo = [
+        //   _data,
+        //   {
+        //     key: "goBao",
+        //     text: `没有${this.selectTarget.walletName}？`,
+        //     hasLink: true,
+        //     dataObj: {
+        //       src: "https://www.gamewallet.asia/",
+        //       text: "立即申请"
+        //     }
+        //   }
+        // ];
+
         let _data = {
           key: "goBao",
-          text: `试试`,
-          hasCallback: true,
-          dataObj: {
-            text: bindingMode ? `扫码绑定` : `一键绑定`,
-            cb: () => {
-              if (bindingMode) {
-                this.selectTarget.oneClickBindingMode = false;
-              } else {
-                this.selectTarget.oneClickBindingMode = true;
-              }
-            }
-          }
+          text: `请使用扫码绑定`
         };
 
         this.walletTipInfo = [
           _data,
           {
             key: "goBao",
-            text: `没有${this.selectTarget.walletName}？`,
+            text: `没有${this.selectTarget.walletName}帐号？`,
             hasLink: true,
             dataObj: {
               src: "https://www.gamewallet.asia/",
