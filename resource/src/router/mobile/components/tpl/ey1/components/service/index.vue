b1785cd69072434af07e7ef2e5ed5dd6add8bb77
tina96_chen <tina96_chen@mail.chungyo.net>
2021-04-09 10:39:30 +0800

    [MOD]億元 修改企鵝客服文字顏色

--- resource/src/router/mobile/components/tpl/ey1/components/service/index.vue
+++ resource/src/router/mobile/components/tpl/ey1/components/service/index.vue
@@ -384,7 +384,7 @@ div.container {
   }
 }
 
-@mixin info-card($color1, $color2, $backgroundImg) {
+@mixin info-card($color1, $color2, $backgroundImg, $smallTitleColor) {
   color: white;
   background: linear-gradient(to left, $color1, $color2);
   box-shadow: 0px 20px 40px 0px rgba(0, 0, 0, 0.15);
@@ -415,7 +415,7 @@ div.container {
     }
 
     > div:nth-child(2) {
-      color: hsla(0, 0%, 100%, 0.5);
+      color: $smallTitleColor;
     }
 
     > div > img {
@@ -429,7 +429,8 @@ div.container {
   @include info-card(
     #f6d2bd,
     #e5997a,
-    "/static/image/common/service/bg_service01.png"
+    "/static/image/common/service/bg_service01.png",
+    hsla(0, 0%, 100%, 0.5)
   );
 }
 
@@ -437,7 +438,8 @@ div.container {
   @include info-card(
     #f9c4c7,
     #e56b6e,
-    "/static/image/common/service/bg_service021.png"
+    "/static/image/common/service/bg_service021.png",
+    #ffffff
   );
 }
 
