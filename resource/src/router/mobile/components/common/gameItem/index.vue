09632385b79002ebcc688771cbdd339fa657db5b
俊賢 <chun-hsien@mail.chungyo.net>
2021-05-11 15:45:32 +0800

    [MOD] 外開監聽測試

--- resource/src/router/mobile/components/common/gameItem/index.vue
+++ resource/src/router/mobile/components/common/gameItem/index.vue
@@ -81,6 +81,8 @@ export default {
   },
   data() {
     return {
+      testNewWindows: "",
+      testInterval: null,
       isShowLoading: false,
       RedEnvelopeTouchType: true,
       isFavorite: false // 客端顯示是否最愛
@@ -351,6 +353,15 @@ export default {
       return this.$i18n.t(name);
     }
   },
+  watch: {
+    testNewWindows() {
+      this.testInterval = setInterval(() => {
+        console.log(
+          `this.testNewWindows.colsed is ${this.testNewWindows.colsed}`
+        );
+      }, 1000);
+    }
+  },
   methods: {
     ...mapActions([
       "actionSetFavoriteGame",
@@ -485,7 +496,12 @@ export default {
           }
         }
       };
-      openGame({ vendor, kind, code }, openGameSuccessFunc, openGameFailFunc);
+      openGame(
+        { vendor, kind, code },
+        this,
+        openGameSuccessFunc,
+        openGameFailFunc
+      );
       return;
     },
     /**
