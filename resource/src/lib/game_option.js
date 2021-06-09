a4f891a5184d097bb113ab8ec6712cef7b256e2d
kent_chou <kent_chou@mail.chungyo.net>
2021-06-09 17:04:32 +0800

    [hotfix]bc內嵌

--- resource/src/lib/game_option.js
+++ resource/src/lib/game_option.js
@@ -4,15 +4,26 @@ import store from "@/store";
 // ("500万彩票");
 // ("丝瓜直播");
 // ("PT");
-const embedGameVendorList = ["evo", "allwin", "sigua_ly","lg_yb_card","lg_yb_casino","jdb"];
-const checkVersion = function () {
-    var agent = window.navigator.userAgent,
-    start = agent.indexOf( 'OS' );
-    if( ( agent.indexOf('iPhone' ) > -1 || agent.indexOf( 'iPad' ) > -1 ) && start > -1 ){
-        return window.Number( agent.substr( start + 3, 3 ).replace( '_', '.' ) );
-    }
-    return 0;
-}
+const embedGameVendorList = [
+  "evo",
+  "allwin",
+  "sigua_ly",
+  "lg_yb_card",
+  "lg_yb_casino",
+  "jdb",
+  "bc"
+];
+const checkVersion = function() {
+  var agent = window.navigator.userAgent,
+    start = agent.indexOf("OS");
+  if (
+    (agent.indexOf("iPhone") > -1 || agent.indexOf("iPad") > -1) &&
+    start > -1
+  ) {
+    return window.Number(agent.substr(start + 3, 3).replace("_", "."));
+  }
+  return 0;
+};
 export const getEmbedGameVendor = vendor => {
   if (!vendor) {
     return null;
@@ -21,7 +32,7 @@ export const getEmbedGameVendor = vendor => {
   let is = embedGameVendorList.includes(vendor);
 
   let version = checkVersion();
-  console.log('IOS VERSION:',version);
+  console.log("IOS VERSION:", version);
 
   if (!is) {
     return null;
