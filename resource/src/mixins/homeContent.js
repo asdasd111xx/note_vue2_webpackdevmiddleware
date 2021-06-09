946de459b6f7679ce631e610f4d33d4aaa0d04d1
俊賢 <chun-hsien@mail.chungyo.net>
2021-06-09 09:18:28 +0800

    [MOD] 修改左側首頁預設

--- resource/src/mixins/homeContent.js
+++ resource/src/mixins/homeContent.js
@@ -200,19 +200,19 @@ export default {
       this.isReceive = true;
       setTimeout(() => {
         this.onResize();
-        let defaultType =
-          this.siteConfig.MOBILE_WEB_TPL === "porn1" ? "welfare" : "all";
-        if (localStorage.getItem("home-menu-type")) {
-          defaultType = localStorage.getItem("home-menu-type");
-        }
+        // let defaultType =
+        //   this.siteConfig.MOBILE_WEB_TPL === "porn1" ? "welfare" : "all";
+        // if (localStorage.getItem("home-menu-type")) {
+        //   defaultType = localStorage.getItem("home-menu-type");
+        // }
 
-        let defaultIndex = this.typeList.findIndex(type => {
-          return type.icon.toLowerCase() === defaultType.toLowerCase();
-        });
+        // let defaultIndex = this.typeList.findIndex(type => {
+        //   return type.icon.toLowerCase() === defaultType.toLowerCase();
+        // });
 
-        defaultIndex = defaultIndex >= 0 ? defaultIndex : 0;
-
-        const selectIndex = this.typeList.length / 3 + defaultIndex;
+        // defaultIndex = defaultIndex >= 0 ? defaultIndex : 0;
+        //0616 預設選項為list 第一個
+        const selectIndex = this.typeList.length / 3;
         this.onChangeSelectIndex(selectIndex);
         this.isShow = true;
       }, 300);
