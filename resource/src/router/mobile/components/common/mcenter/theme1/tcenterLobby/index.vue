47db4e929171786266f45146614b01379d2e1fe8
俊賢 <chun-hsien@mail.chungyo.net>
2021-07-15 15:00:12 +0800

    Revert "[MOD] 鴨脖推廣大廳"
    This reverts commit 1ae75cf437343a9820a39cc54e864df1d211dc0d.

--- resource/src/router/mobile/components/common/mcenter/theme1/tcenterLobby/index.vue
+++ resource/src/router/mobile/components/common/mcenter/theme1/tcenterLobby/index.vue
@@ -12,42 +12,25 @@
       />
       <div :class="$style['title-label']">我的推广</div>
       <div
-        v-if="!rewardOnlyLocal"
         :class="$style['title-commission']"
         @click="$router.push('/mobile/mcenter/tcenter/commission/summary')"
       >
-        盈亏返利
+        收益概况
       </div>
     </div>
     <div :class="$style['main']">
       <!-- <div :class="[$style['top-bg']]"></div> -->
       <div v-if="friendsStatistics" :class="$style['top-data']">
-        <div :class="$style['list-data']">
-          <div :class="$style['list-name']">
-            {{ isShowRebate ? "最新可领实返" : "今日已领返利" }}
-            <div
-              v-if="isShowRebate"
-              :class="$style['list-btn']"
-              @click="$router.push('/mobile/mcenter/tcenter/commission/rebate')"
-            >
-              {{ getRebateText }}
-            </div>
-          </div>
-          <div :class="$style['list-value']">
-            {{ isShowRebate ? rebateCount : summary.today.amount || "--" }}
-          </div>
+        <div
+          v-if="isShowRebate"
+          :class="$style['list-data']"
+          @click="$router.push('/mobile/mcenter/tcenter/commission/rebate')"
+        >
+          <div :class="$style['list-name']">实时返利</div>
+          <div :class="$style['list-value']">领取 ></div>
         </div>
         <div :class="$style['list-data']">
-          <div :class="$style['list-name']">
-            今日有效投注
-            <div
-              v-if="isShowRebate"
-              :class="$style['list-btn']"
-              @click="$router.push('/mobile/mcenter/tcenter/commission/rebate')"
-            >
-              详情
-            </div>
-          </div>
+          <div :class="$style['list-name']">今日有效投注</div>
           <div :class="$style['list-value']">
             {{ isShowRebate ? subValidBet : friendsStatistics.valid_bet }}
           </div>
@@ -64,115 +47,32 @@
         </div>
       </div>
       <div v-if="friendsStatistics" :class="$style['regist-data']">
-        <div :class="$style['all-regist-title']">
-          <div :class="$style['regist-title-bar']"></div>
-          <div :class="$style['regist-title-text']">全部下级概况</div>
-        </div>
         <div :class="$style['regist-content']">
-          <div :class="$style['regist-title']">今日注册人数</div>
+          <div :class="$style['regist-title']">今日注册</div>
           <div :class="$style['regist-value']">
             {{ formatToPrice(friendsStatistics.today_register) }}
           </div>
         </div>
         <div :class="$style['regist-content']">
-          <div :class="$style['regist-title']">今日充值人数</div>
+          <div :class="$style['regist-title']">本月注册</div>
           <div :class="$style['regist-value']">
-            {{ formatToPrice(friendsStatistics.today_has_deposit) }}
+            {{ formatToPrice(friendsStatistics.month_register) }}
           </div>
         </div>
         <div :class="$style['regist-content']">
-          <div :class="$style['regist-title']">一级好友人数</div>
-          <div :class="$style['regist-value']">
-            {{ formatToPrice(levelList[0].total) }}
-          </div>
-        </div>
-        <div :class="$style['regist-content']">
-          <div :class="$style['regist-title']">全部下级人数</div>
+          <div :class="$style['regist-title']">注册总数</div>
           <div :class="$style['regist-value']">
             {{ formatToPrice(friendsStatistics.user_count) }}
           </div>
         </div>
         <div :class="$style['regist-content']">
-          <div :class="$style['regist-title']">今日投注会员</div>
+          <div :class="$style['regist-title']">今日损益</div>
           <div :class="$style['regist-value']">
-            {{ formatToPrice(friendsStatistics.today_has_bet) }}
-          </div>
-        </div>
-        <div :class="$style['regist-content']">
-          <div :class="$style['regist-title']">今日损益金额</div>
-          <div
-            :class="[
-              $style['regist-value'],
-              { [$style['game-lose']]: Number(friendsStatistics.payoff) < 0 }
-            ]"
-          >
             {{ getNoRoundText(friendsStatistics.payoff) }}
           </div>
         </div>
       </div>
-      <div :class="[$style['special']]">
-        <div :class="[$style['special-content']]">
-          <div
-            :class="[$style['special-data'], [$style['first']]]"
-            :style="
-              transPointType ? { width: `${100 / 3}%` } : { width: `48%` }
-            "
-            @click="$router.push(data.path)"
-          >
-            <div :class="[$style['special-data-img']]">
-              <img
-                :class="[$style['img-icon']]"
-                :src="
-                  $getCdnPath(
-                    `/static/image/${themeTPL}/mcenter/tcenter/btn_group.png`
-                  )
-                "
-              />
-            </div>
-            <div>团队管理</div>
-          </div>
-          <div
-            :class="[$style['special-data']]"
-            :style="
-              transPointType ? { width: `${100 / 3}%` } : { width: `48%` }
-            "
-            @click="$router.push(data.path)"
-          >
-            <div :class="[$style['special-data-img']]">
-              <img
-                :class="[$style['img-icon']]"
-                :src="
-                  $getCdnPath(
-                    `/static/image/${themeTPL}/mcenter/tcenter/btn_reward.png`
-                  )
-                "
-              />
-            </div>
-            <div>返利管理</div>
-          </div>
-          <div
-            v-if="transPointType"
-            :class="[$style['special-data']]"
-            :style="
-              transPointType ? { width: `${100 / 3}%` } : { width: `48%` }
-            "
-            @click="$router.push(data.path)"
-          >
-            <div :class="[$style['special-data-img']]">
-              <img
-                :class="[$style['img-icon']]"
-                :src="
-                  $getCdnPath(
-                    `/static/image/${themeTPL}/mcenter/tcenter/btn_transfer.png`
-                  )
-                "
-              />
-            </div>
-            <div>转点管理</div>
-          </div>
-        </div>
-      </div>
-      <!-- <div :class="$style['rebate-content']">
+      <div :class="$style['rebate-content']">
         <img
           :class="$style['img-icon']"
           :src="
@@ -191,14 +91,14 @@
           "
           @click="$router.push('/mobile/mcenter/tcenter/commission/record')"
         />
-      </div> -->
+      </div>
       <div
         :class="[
           $style['special'],
           { [$style['less']]: specialData.length < 3 }
         ]"
       >
-        <!-- <div :class="$style['special-title']">特色功能</div> -->
+        <div :class="$style['special-title']">特色功能</div>
         <div
           :class="[
             $style['special-content'],
@@ -253,9 +153,6 @@
 import { mapGetters } from "vuex";
 import friendsStatistics from "@/mixins/mcenter/management/friendsStatistics";
 import bbosRequest from "@/api/bbosRequest";
-import goLangApiRequest from "@/api/goLangApiRequest";
-import { API_COMMISSION_SUMMARY } from "@/config/api";
-import ajax from "@/lib/ajax";
 export default {
   components: {},
   mixins: [friendsStatistics],
@@ -264,13 +161,6 @@ export default {
       subValidBet: 0,
       subUserCount: 0,
       isShowRebate: true,
-      getRebateText: "",
-      rebateCount: "",
-      levelList: [[{ total: 0 }]],
-      allTotal: [],
-      transPointType: false,
-      summary: null,
-      immediateData: [],
       specialList: [
         {
           showType: true,
@@ -301,7 +191,6 @@ export default {
   },
   created() {
     this.getRebateSwitch();
-    this.getLevelList();
 
     this.specialData.forEach(element => {
       if (element.name === "推荐礼金") {
@@ -325,16 +214,6 @@ export default {
     },
     themeTPL() {
       return this.siteConfig.MOBILE_WEB_TPL;
-    },
-    /**
-     * 返利是否只使用本站
-     */
-    rewardOnlyLocal() {
-      return (
-        this.memInfo.config.wage &&
-        this.memInfo.config.wage.length === 1 &&
-        this.memInfo.config.wage[0] === "local"
-      );
     }
   },
   mounted() {},
@@ -375,105 +254,9 @@ export default {
             this.subUserCount = response.data.total.valid_bet.sub_user_count
               ? this.formatToPrice(response.data.total.valid_bet.sub_user_count)
               : "--";
-            let dataArray = [];
-            if (this.themeTPL === "ey1") {
-              // this.dispatch_hour = response.data.auto_dispatch_hour;
-              dataArray = response.data.entries;
-            } else {
-              // this.dispatch_hour = response.data.ret.auto_dispatch_hour;
-              dataArray = response.data.ret.entries;
-            }
-            this.immediateData = dataArray.length > 0 ? dataArray[0] : null;
-            if (this.immediateData && this.immediateData.state === 1) {
-              // 可領
-              this.getRebateText = "领取";
-              this.rebateCount = this.getNoRoundText(
-                this.immediateData.self_min_limit
-              );
-            } else {
-              // 已达上限
-              this.getRebateText = "查看";
-              this.rebateCount = "0.00";
-            }
-            // 測試資料
-            // this.immediateData = [
-            //     {
-            //         period: "20200421",
-            //         start_at: "2020-04-21T12:00:00+0800",
-            //         end_at: "2020-04-21T07:15:18+0800",
-            //         sub_valid_bet: "14875039.6179",
-            //         sub_profit: "0.00",
-            //         state: 1,
-            //         self_times: 10,
-            //         self_min_limit: "10",
-            //         type: 1,
-            //         amount: "200.00"
-            //     }
-            // ];
-          } else {
-            this.getSummary();
           }
+
           return;
-        }
-      });
-    },
-    /**
-     * 取得各級好友的資訊
-     * @method getLevelList
-     */
-    getLevelList() {
-      this.levelList = [];
-
-      return goLangApiRequest({
-        method: "get",
-        url: `${this.siteConfig.YABO_GOLANG_API_DOMAIN}/xbb/Stats/Player/Friends/TransReport`,
-        params: {
-          lang: "zh-cn",
-          startAt: new Date(),
-          endAt: new Date()
-        }
-      })
-        .then(res => {
-          const { data, status, errorCode } = res;
-          if (status !== "000" || errorCode !== "00") {
-            return;
-          }
-
-          this.allTotal = {
-            total: 0,
-            register: 0,
-            deposit: 0,
-            withdraw: 0
-          };
-
-          this.levelList = data.map(info => {
-            this.allTotal.total += +info.total;
-            this.allTotal.register += +info.register;
-            this.allTotal.deposit += +info.deposit;
-            this.allTotal.withdraw += +info.withdraw;
-            return info;
-          });
-        })
-        .catch(error => {
-          const { msg, code } = error.response.data;
-          this.actionSetGlobalMessage({
-            msg
-          });
-        });
-    },
-    /**
-     * 取得收益概況
-     */
-    getSummary() {
-      ajax({
-        method: "get",
-        url: API_COMMISSION_SUMMARY,
-        success: ({ result, ret }) => {
-          if (result !== "ok") {
-            return;
-          }
-
-          this.summary = ret;
         }
       });
     }
