4ffc1f9ebd62bcbecb1fdd281eb418fe8c535a62
tina96_chen <tina96_chen@mail.chungyo.net>
2021-08-18 14:40:33 +0800

    [MOD]修正已達上限

--- resource/src/router/mobile/components/common/mcenter/theme1/tcenter/components/commission/components/commissionRebates/index.vue
+++ resource/src/router/mobile/components/common/mcenter/theme1/tcenter/components/commission/components/commissionRebates/index.vue
@@ -49,14 +49,9 @@
 
           <div :class="[$style['rebate-body'], { [$style.pathbody]: path }]">
             <div
-              v-if="
-                (path &&
-                  caculateList.state !== 3 &&
-                  caculateList.self_times !== 0) ||
-                  (path && caculateList.state === 3)
-              "
-              :class="$style['detail-content']"
-            >
+              v-if="caculateList.state === 3 && caculateList.self_times === 0"
+            ></div>
+            <div v-else :class="$style['detail-content']">
               <span :class="$style['content-left']">
                 结算区间
               </span>
