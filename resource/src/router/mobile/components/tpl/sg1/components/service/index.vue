6c0f276b66a6c8877a9e74fe386bc7b7941f0358
Chris_Huang <chris_huang@mail.chungyo.net>
2021-10-15 14:48:20 +0800

    [MOD] 永久網址style

--- resource/src/router/mobile/components/tpl/sg1/components/service/index.vue
+++ resource/src/router/mobile/components/tpl/sg1/components/service/index.vue
@@ -569,28 +569,22 @@ div.container {
 
   .title-img {
     width: 100%;
-    height: 80px;
 
     img {
       width: 100%;
-      height: 100%;
     }
   }
 
   .content-cell {
-    margin-top: 12px;
-
     span {
       padding: 2px 0;
     }
 
     .content-img {
       width: 100%;
-      height: 100px;
 
       img {
         width: 100%;
-        height: 100%;
       }
     }
 
