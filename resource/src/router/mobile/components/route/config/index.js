547b038fa092298383d5ae4e3067054550f15512
tina96_chen <tina96_chen@mail.chungyo.net>
2021-10-27 08:00:14 +0800

    [MOD]鸭博改币发 文字

--- resource/src/router/mobile/components/route/config/index.js
+++ resource/src/router/mobile/components/route/config/index.js
@@ -13,7 +13,7 @@ export default merge(
       rootEy1: () => import(/* webpackChunkName: 'rootEy1' */ "../../tpl/ey1/"),
       rootSg1: () => import(/* webpackChunkName: 'rootSg1' */ "../../tpl/sg1/"),
 
-      // 仿鸭博
+      // 仿币发BIFA
       rootAobo1: () =>
         import(/* webpackChunkName: 'rootSg1' */ "../../tpl/aobo1/"),
       rootSp1: () => import(/* webpackChunkName: 'rootSp1' */ "../../tpl/sp1/")
