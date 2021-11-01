bc341bc2b42d24c2ce210ebefec86358a42d49ba
kent_chou <kent_chou@mail.chungyo.net>
2021-11-01 11:55:49 +0800

    [MOD]直播 鑽石頁面

--- resource/src/router/mobile/components/tpl/sg1/components/mcenter/components/live/diamond/index.vue
+++ resource/src/router/mobile/components/tpl/sg1/components/mcenter/components/live/diamond/index.vue
@@ -1,6 +1,51 @@
 <template>
   <mobile-container :header-config="headerConfig" :has-footer="false">
     <div slot="content" :class="$style['']">
+      <div :class="$style['account-wrap']">
+        <img :src="$getCdnPath(`/static/image/sg1/live/icon_gold.png`)" />
+      </div>
+      <div :class="$style['buy-diamond-wrap']">
+        <div :class="$style['title']">
+          兑换钻石数量
+        </div>
+        <div :class="$style['price-wrap']">
+          <div
+            v-for="(item, key) in exchangeRateList"
+            :class="[
+              $style['price-cell'],
+              { [$style['selected']]: item.amount === currentSelRate.amount }
+            ]"
+            @click="!item.loading ? selectedRate(item) : {}"
+            :key="key"
+          >
+            <template v-if="!item.loading">
+              <div :class="$style['price']">{{ `¥ ${item.amount}` }}</div>
+              <div :class="$style['num']">
+                <img
+                  :src="
+                    $getCdnPath(
+                      `/static/image/sg1/mcenter/live/ic_diamond_s.png`
+                    )
+                  "
+                />
+                <div>
+                  {{ `${item.diamond}` }}
+                </div>
+              </div>
+              <img
+                v-if="item.amount === currentSelRate.amount"
+                :class="$style['amount-active']"
+                :src="$getCdnPath(`/static/image/common/select_active.png`)"
+              />
+            </template>
+            <template v-else>
+              <div :class="$style['loading']" />
+              <div :class="$style['loading']" />
+            </template>
+          </div>
+        </div>
+      </div>
+
       <page-loading :is-show="isLoading" />
     </div>
   </mobile-container>
@@ -24,7 +69,8 @@ export default {
       isLoading: true,
       diamondTotal: {},
       diamondRemind: {},
-      exchangeRateList: {}
+      exchangeRateList: {},
+      currentSelRate: {}
     };
   },
   computed: {
@@ -76,4 +122,89 @@ export default {
 </script>
 <style lang="scss" module>
 @import "~@/css/variable.scss";
+
+.buy-diamond-wrap {
+  width: 100%;
+  margin-top: 7px;
+  background: #ffffff;
+  padding: 8px 17px;
+
+  .title {
+    font-size: 14px;
+    font-family: Microsoft JhengHei, Microsoft JhengHei-Regular;
+    font-weight: 400;
+    text-align: left;
+    color: #222222;
+  }
+}
+
+.price-wrap {
+  position: relative;
+  padding: 0;
+  margin-top: 3px;
+}
+
+.price-cell {
+  font-family: Arial, Arial-Regular;
+  font-weight: 400;
+  text-align: center;
+  position: relative;
+  display: inline-block;
+  height: 40px;
+  background: #ffffff;
+  border: 1px solid #eeeeee;
+  border-radius: 3px;
+  width: calc(25% - 6px);
+
+  &:not(:nth-child(4n + 1)) {
+    margin-left: 6px;
+  }
+
+  &.selected {
+    border: 1px solid #d1b79c;
+    background: linear-gradient(-45deg, #d1b79c 10px, rgba(255, 255, 255, 0) 0);
+
+    .price {
+      color: #d1b79c;
+    }
+
+    .num {
+      color: #36e6d2;
+    }
+
+    .amount-active {
+      display: block;
+      width: 7px;
+      height: 5px;
+      position: absolute;
+      right: 1px;
+      bottom: 3px;
+    }
+  }
+
+  .price {
+    color: #2d1212;
+    font-size: 13px;
+  }
+
+  .num {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #aaaaaa;
+    font-size: 12px;
+
+    > img {
+      width: 12px;
+      height: 12px;
+      vertical-align: bottom;
+    }
+  }
+
+  .loading {
+    margin: 6px 11px;
+    background: #eeeeee;
+    height: calc(50% - 9px);
+  }
+}
 </style>
