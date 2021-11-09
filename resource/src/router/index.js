24efcdbc16b7ce7e68606738c201c432cd586783
kent_chou <kent_chou@mail.chungyo.net>
2021-11-09 10:55:56 +0800

    [MOD]route /a/code

--- resource/src/router/index.js
+++ resource/src/router/index.js
@@ -1,3 +1,5 @@
+import { getCookie, setCookie } from "@/lib/cookie";
+
 import Router from "vue-router";
 import Vue from "vue";
 import agent from "./agent";
@@ -12,7 +14,7 @@ import popControl from "./popcontrol";
 import staticService from "./static/";
 import timeout from "./timeout";
 import upup from "./upup";
-import { getCookie, setCookie } from "@/lib/cookie";
+
 // prevent NavigationDuplicated error see: https://github.com/vuejs/vue-router/issues/2881
 const originalPush = Router.prototype.push;
 Router.prototype.push = function push(location, onResolve, onReject) {
@@ -57,6 +59,21 @@ export default new Router({
 
         next("/mobile");
         return;
+      }
+    },
+    {
+      path: "/a/:code",
+      beforeEnter(to, from, next) {
+        const code = to.params.code;
+        if (code) {
+          setCookie("cid", "");
+          setCookie("aid", "");
+          localStorage.removeItem("aid");
+          window.RESET_LOCAL_SETTING();
+          window.RESET_MEM_SETTING();
+        }
+        localStorage.setItem("promotionCode", code || "");
+        next("/mobile");
       }
     },
     popControl,
