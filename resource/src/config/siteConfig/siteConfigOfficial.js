44b3f4b0d34849747c561f8f488d229eb96e4245
俊賢 <chun-hsien@mail.chungyo.net>
2021-11-09 14:57:25 +0800

    [MOD] 測試澳博demo domain

--- resource/src/config/siteConfig/siteConfigOfficial.js
+++ resource/src/config/siteConfig/siteConfigOfficial.js
@@ -65,7 +65,7 @@ export const site_69 = {
   SITE_NAME: "鸭博娱乐",
   YABO_API_DOMAIN: "https://yaboapi.canarycherrytart.com/api",
   // YABO_GOLANG_API_DOMAIN: "https://yaboapi.canarycherrytart.com/api-v2",
-  YABO_GOLANG_API_DOMAIN: "https://yaboapi.pcxgqij.cn:9443/api-v2",
+  YABO_GOLANG_API_DOMAIN: "https://yaboapi.lysywh.cn:9443/api-v2",
   ACTIVES_BOUNS_WEBSOCKETV2:
     "wss://yaboapi.iguanagingercake.com/wsn/promosocket?",
   PORN_CONFIG: {
@@ -266,7 +266,7 @@ export const site_93 = {
   SITE_NAME: "澳博国际",
   YABO_API_DOMAIN: "https://yaboapi.canarycherrytart.com/api",
   // YABO_GOLANG_API_DOMAIN: "https://yaboapi.canarycherrytart.com/api-v2",
-  YABO_GOLANG_API_DOMAIN: "https://yaboapi.lysywh.cn:9443/api-v2",
+  YABO_GOLANG_API_DOMAIN: "https://yaboapi.pcxgqij.cn:9443/api-v2",
   ACTIVES_BOUNS_WEBSOCKETV2:
     "wss://yaboapi.iguanagingercake.com/wsn/promosocket?",
   PORN_CONFIG: {
