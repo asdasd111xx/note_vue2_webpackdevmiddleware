b85885e1a931cb884ab448f6a838aeae2a6e0526
Chris_Huang <chris_huang@mail.chungyo.net>
2021-12-17 10:38:53 +0800

    [MOD]客服 永久網址文字顏色

--- resource/src/router/mobile/components/tpl/porn1/components/service/index.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/service/index.vue
@@ -775,7 +775,7 @@ div.container {
   .title,
   .content {
     margin: 0 17px;
-    color: var(--main_text_color2);
+    color: var(--member_text_color3);
     font-size: 12px;
   }
 
@@ -806,7 +806,7 @@ div.container {
       margin: 10px auto;
       padding: 0 0 0 40px;
       background: #ffffff;
-      border: 1px solid var(--main_text_color2);
+      border: 1px solid var(--member_text_color3);
       border-radius: 10px;
 
       &::before {
