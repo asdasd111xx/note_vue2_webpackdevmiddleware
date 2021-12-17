6c1ccedd467735742052858d8722ff5832a54582
Chris_Huang <chris_huang@mail.chungyo.net>
2021-12-17 11:14:48 +0800

    [MOD]個資欄位＆說明文字顏色

--- resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/accountData/components/index.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/mcenter/components/accountData/components/index.vue
@@ -111,7 +111,7 @@ export default {
 .mcenter-avatar-wrap {
   height: 120px;
   background-color: $main_white_color1;
-  color: var(--main_text_color3);
+  color: var(--member_text_color4);
   padding: 15px;
   border-bottom: 1px solid #eee;
 
