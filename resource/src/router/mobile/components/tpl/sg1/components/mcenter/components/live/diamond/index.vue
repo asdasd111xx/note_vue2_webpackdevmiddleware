65dbf12fd548a48adcc924a46e84d5aeb8c210ce
俊賢 <chun-hsien@mail.chungyo.net>
2022-01-07 17:14:38 +0800

    [MOD] 新增預設鑽石選取

--- resource/src/router/mobile/components/tpl/sg1/components/mcenter/components/live/diamond/index.vue
+++ resource/src/router/mobile/components/tpl/sg1/components/mcenter/components/live/diamond/index.vue
@@ -362,6 +362,7 @@ export default {
         this.isLoading = false;
         if (data && data.result && data.result.exchange_rate_list) {
           this.exchangeRateList = data.result.exchange_rate_list;
+          this.selectedRate(this.exchangeRateList[0]);
         } else {
           this.exchangeRateList = [];
         }
