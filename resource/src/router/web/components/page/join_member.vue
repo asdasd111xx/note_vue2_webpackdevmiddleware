11f5a87044c162188d5d3b304ad81262219269a4
俊賢 <chun-hsien@mail.chungyo.net>
2022-01-18 17:04:00 +0800

    [MOD] 簡化註冊信箱電話驗證碼判斷

--- resource/src/router/web/components/page/join_member.vue
+++ resource/src/router/web/components/page/join_member.vue
@@ -1531,12 +1531,8 @@ export default {
         aid: this.aid || getCookie("aid") || localStorage.getItem("aid") || "",
         speedy: false, //檢查是否唯一
         code: localStorage.getItem("x-code") || "",
-        phone_keyring: this.register_phone_keyring !== ""
-          ? this.register_phone_keyring
-          : "",
-        email_keyring: this.register_email_keyring !== ""
-          ? this.register_email_keyring
-          : ""
+        phone_keyring: this.register_phone_keyring ,
+        email_keyring: this.register_email_keyring 
       };
 
       const self = this;
