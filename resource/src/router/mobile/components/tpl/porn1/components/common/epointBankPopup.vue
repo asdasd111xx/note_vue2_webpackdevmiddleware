ae9ac362b258b050eda1e639c2f0cbd3a9e45ad3
Chris_Huang <chris_huang@mail.chungyo.net>
2022-01-24 14:17:50 +0800

    [MOD]充值,提現,綁卡頁 細節顏色

--- resource/src/router/mobile/components/tpl/porn1/components/common/epointBankPopup.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/common/epointBankPopup.vue
@@ -134,7 +134,8 @@ export default {
     position: absolute;
     text-align: center;
     width: 40px;
-    color: var(--addcard_popup_prev_text_color);
+    // color: var(--addcard_popup_prev_text_color);
+    color: #6aaaf5;
   }
 
   .title {
@@ -164,7 +165,8 @@ export default {
     top: 50%;
     width: 15px;
     height: 15px;
-    color: var(--addcard_popup_check_color);
+    // color: var(--addcard_popup_check_color);
+    color: #6aaaf5;
     transform: translateY(-50%);
   }
 }
