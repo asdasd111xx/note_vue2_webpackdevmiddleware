fd832dbf7f0da1f27af3948e96e5b16aa7e23707
kent_chou <kent_chou@mail.chungyo.net>
2022-02-22 12:22:17 +0800

    [MOD]reset live-iframe-launch-home

--- resource/src/router/mobile/components/tpl/sg1/components/launch/index.vue
+++ resource/src/router/mobile/components/tpl/sg1/components/launch/index.vue
@@ -3,12 +3,15 @@
 </template>
 
 <script>
-import launch from '@/router/mobile/components/common/launch'
+import launch from "@/router/mobile/components/common/launch";
 export default {
   components: {
     launch
+  },
+  created() {
+    // 泡泡直播首頁
+    localStorage.removeItem("live-iframe-launch-home");
   }
 };
 </script>
-<style lang="scss" module>
-</style>
+<style lang="scss" module></style>
