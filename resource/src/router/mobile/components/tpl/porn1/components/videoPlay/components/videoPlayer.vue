854eba6ee2a6aa1f40af4371436f96dd777e8a68
俊賢 <chun-hsien@mail.chungyo.net>
2022-02-23 09:51:04 +0800

    [MOD] 修改廣告播放事件

--- resource/src/router/mobile/components/tpl/porn1/components/videoPlay/components/videoPlayer.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/videoPlay/components/videoPlayer.vue
@@ -483,6 +483,9 @@ export default {
                     Number(this.mission.ActionType) === 6;
                 }
                 this.isFULL = true;
+                if (this.adSwitch) {
+                  this.onSend("ADSTART");
+                }
                 // bonunsDialog.isShow = true;
                 // this.dialogType = 'tips-full';
                 // this.playerPause();
