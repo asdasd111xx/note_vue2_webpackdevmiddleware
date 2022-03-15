19fab5fd67422baec58033cb9f42aeeee991d5ec
俊賢 <chun-hsien@mail.chungyo.net>
2022-03-15 15:00:00 +0800

    [MOD] 修正彩金當日領滿流程

--- resource/src/router/mobile/components/tpl/porn1/components/videoPlay/components/videoPlayer.vue
+++ resource/src/router/mobile/components/tpl/porn1/components/videoPlay/components/videoPlayer.vue
@@ -486,8 +486,11 @@ export default {
                     Number(this.mission.ActionType) === 6;
                 }
                 this.isFULL = true;
-                if (data.FullMode === 1 && this.adSwitch) {
-                  this.onSend("ADSTART");
+                if (data.FullMode === 1) {
+                  this.$nextTick(() => bonunsProcess.playCueTime("stop"));
+                  if (this.adSwitch) {
+                    this.onSend("ADSTART");
+                  }
                 }
                 // bonunsDialog.isShow = true;
                 // this.dialogType = 'tips-full';
