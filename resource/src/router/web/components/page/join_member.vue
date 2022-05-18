e50b04474da2810f44e3ebdc9d632fec6b6aab04
Chris_Huang <chris_huang@mail.chungyo.net>
2022-05-18 17:02:35 +0800

    [MOD]手機註冊無效驗證碼位置

--- resource/src/router/web/components/page/join_member.vue
+++ resource/src/router/web/components/page/join_member.vue
@@ -2266,9 +2266,7 @@ export default {
           this.allValue.captcha_text = "";
 
           if (res.status !== "000") {
-            if (res.code === "C20077") {
-              this.allTip["phonettl"] = res.msg;
-            } else if (res.errors && res.errors.phone) {
+            if (res.errors && res.errors.phone) {
               this.allTip["mphone"] = res.errors.phone;
             } else {
               this.mobileJoinErrMag = res.msg;
