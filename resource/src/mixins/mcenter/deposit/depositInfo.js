f1b46f3c5f3aadb18b252261b2627476cf48bae5
Chris_Huang <chris_huang@mail.chungyo.net>
2022-05-18 10:44:59 +0800

    [MOD] api

--- resource/src/mixins/mcenter/deposit/depositInfo.js
+++ resource/src/mixins/mcenter/deposit/depositInfo.js
@@ -326,8 +326,7 @@ export default {
           pay_username: this.speedField.depositName,
           method: this.speedField.depositMethod,
           branch: this.speedField.bankBranch,
-          sn: this.speedField.serialNumber,
-          pay_url: ""
+          sn: this.speedField.serialNumber
         };
         return axios({
           method: "put",
