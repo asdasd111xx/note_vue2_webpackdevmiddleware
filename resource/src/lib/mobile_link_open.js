fb595b1321f53b4e53c528a7e9d200d2dd0e65f9
TSAI YIXUAN <jason_tsai@mail.chungyo.net>
2022-05-26 14:58:47 +0800

    Revert "Revert "[MOD]優小蜜事件跳轉邏輯""
    This reverts commit 6f045320fe61e163b7854bf205e47229217e7cb8.

--- resource/src/lib/mobile_link_open.js
+++ resource/src/lib/mobile_link_open.js
@@ -274,6 +274,11 @@ export default async target => {
         return;
 
       case "binding-card":
+        if (!store.state.loginStatus) {
+          router.push("/mobile/login");
+          return;
+        }
+
         router.push(
           `/mobile/mcenter/bankCard?redirect=promotion&type=bankCard`
         );
